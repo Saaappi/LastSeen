579a64b3655025f87353c78be64a3134a7620bd6
DrOxlotus <alex.metz1024@gmail.com>
2019-04-16 06:12:40 -0500

    Removed the retired item quality table.

--- DB/DB.lua
+++ DB/DB.lua
@@ -5,23 +5,10 @@
 
 local addonName, addonTable = ...;
 
-local ItemQualities = {
-	[0] = "9d9d9d",
-	[1] = "ffffff",
-	[2] = "1eff00",
-	[3] = "0070dd",
-	[4] = "a335ee",
-	[5] = "ff8000",
-	[6] = "e6cc80",
-	[7] = "e6cc80",
-	[8] = "00ccff",
-};
-
 local LastSeenItems = {};
 local LastSeenIgnore = {};
 local ItemIDCache = {};
 
-addonTable.ItemQualities = ItemQualities;
 addonTable.LastSeen = IsAddOnLoaded(addonName);
 addonTable.LastSeenItems = LastSeenItems;
 addonTable.LastSeenIgnore = LastSeenIgnore;
