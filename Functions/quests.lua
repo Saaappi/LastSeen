0693c397728e25223d7f1b2e15fc51be0e68309d
DrOxlotus <alex.metz1024@gmail.com>
2019-04-23 05:04:27 -0500

    More quest and item fixes.

--- Functions/quests.lua
+++ Functions/quests.lua
@@ -21,7 +21,7 @@ lastSeenNS.QuestChoices = function(questID, today, currentMap)
 			local chosenItemName, _, _, itemRarity, _ = GetQuestItemInfo("choice", i); -- The player chooses this item
 			local rewardItemName, _, _, itemRarity, _ = GetQuestItemInfo("reward", i); -- An item that is given to the player as a reward (they do not choose it)
 			if not lastSeenNS.LastSeenItems[chosenItemName].itemName or not lastSeenNS.LastSeenItems[rewardItemName] then
-				lastSeenNS.LastSeenItems[itemID] = {itemName = chosenItemName, itemLink = lastSeenNS.GetItemLink(chosenItemName), itemRarity = itemRarity, itemType = lastSeenNS.GetItemType(chosenItemName), lootDate = today, source = questTitle, location = lastSeenNS.currentMap};
+				lastSeenNS.LastSeenItems[itemID] = {itemName = chosenItemName, itemLink = lastSeenNS.GetItemLink(chosenItemName), itemRarity = itemRarity, itemType = lastSeenNS.GetItemType(chosenItemName), lootDate = today, source = questTitle, location = currentMap};
 			end
 			if lastSeenNS.hasSeenQuest then
 				if lastSeenNS.LastSeenQuests[questID].completed ~= today then
@@ -29,9 +29,9 @@ lastSeenNS.QuestChoices = function(questID, today, currentMap)
 				end
 			else
 				if chosenItemName ~= nil then
-					lastSeenNS.LastSeenQuests[questID] = {title = questTitle, completed = today, rewards = {reward = chosenItemName}, location = lastSeenNS.currentMap};
+					lastSeenNS.LastSeenQuests[questID] = {title = questTitle, completed = today, rewards = {reward = chosenItemName}, location = currentMap};
 				else
-					lastSeenNS.LastSeenQuests[questID] = {title = questTitle, completed = today, rewards = {reward = rewardItemName}, location = lastSeenNS.currentMap};
+					lastSeenNS.LastSeenQuests[questID] = {title = questTitle, completed = today, rewards = {reward = rewardItemName}, location = currentMap};
 				end
 			end
 			i = i + 1;
@@ -42,7 +42,7 @@ lastSeenNS.QuestChoices = function(questID, today, currentMap)
 				lastSeenNS.LastSeenQuests[questID].completed = today;
 			end
 		else
-			lastSeenNS.LastSeenQuests[questID] = {title = questTitle, completed = today, rewards = 0, location = lastSeenNS.currentMap};
+			lastSeenNS.LastSeenQuests[questID] = {title = questTitle, completed = today, rewards = 0, location = currentMap};
 		end
 	end
 end
