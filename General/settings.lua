84d7adece2c9198bc58e63a64d4f62195926b6fe
DrOxlotus <alex.metz1024@gmail.com>
2019-10-02 00:50:21 -0500

    Cleaned up a now unused label.

--- General/settings.lua
+++ General/settings.lua
@@ -191,14 +191,6 @@ local function SettingsMenu_OnShow()
 		LastSeenTbl.tab1.rankLabel:SetText(L["RANK"] .. LastSeenTbl.GetPlayerRank());
 	end
 	
-	--[[if not LastSeenTbl.tab1.versionLabel then
-		LastSeenTbl.tab1.versionLabel = LastSeenTbl.tab1:CreateFontString(nil, "OVERLAY");
-		LastSeenTbl.tab1.versionLabel:SetFontObject("GameFontHighlight");
-		LastSeenTbl.tab1.versionLabel:SetPoint("TOPRIGHT", LastSeenTbl.tab1.itemsSeenLabel, 265, 0);
-		LastSeenTbl.tab1.versionLabel:SetFont("Fonts\\Arial.ttf", 8);
-		LastSeenTbl.tab1.versionLabel:SetText(L["RELEASE"]);
-	end]]--
-
 	if not LastSeenTbl.tab1.releaseDateLabel then
 		LastSeenTbl.tab1.releaseDateLabel = LastSeenTbl.tab1:CreateFontString(nil, "OVERLAY");
 		LastSeenTbl.tab1.releaseDateLabel:SetFontObject("GameFontHighlight");
