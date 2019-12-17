076842be23fb588751a78695f8fc6ed0b13390d6
DrOxlotus <alex.metz1024@gmail.com>
2019-12-17 02:39:39 -0600

    Removed the unused rare code.

--- Functions/NameplateHandler.lua
+++ Functions/NameplateHandler.lua
@@ -14,34 +14,6 @@ GetPlayerMapPosition = C_Map.GetPlayerMapPosition;
 
 local playerName = UnitName(L["IS_PLAYER"]);
 
---[[
-local function RareSeen(unit, creatureID, seenDate, isInInstance)
-	-- We don't want the sound file to play every time the nameplate is shown. We'll play once a day instead.
-	-- This rare has been seen for the first time on a new day or by a new character.
-	if LastSeenCreaturesDB[creatureID]["seen"] ~= seenDate or LastSeenCreaturesDB[creatureID]["player"] ~= playerName then
-		LastSeenCreaturesDB[creatureID]["seen"] = seenDate;
-		LastSeenCreaturesDB[creatureID]["player"] = playerName;
-		
-		if isInInstance then
-			print(L["ADDON_NAME"] .. L["RARE"] .. " - " .. LastSeenCreaturesDB[creatureID].unitName);
-		else
-			print(L["ADDON_NAME"] .. L["RARE"] .. " - " .. LastSeenCreaturesDB[creatureID].unitName .. " (" .. LastSeenTbl.Round(UnitHealth(unit)/UnitHealthMax(unit), 2) .. "%)");
-		end
-		
-		if not LastSeenTbl.doNotPlayRareSound then
-			PlaySoundFile(LastSeenSettingsCacheDB.rareSoundID);
-		end
-
-		if LastSeenTbl["autoMark"] == false then
-			if unit then
-				if GetRaidTargetIndex(unit) == nil then
-					SetRaidTarget(unit, 8);
-				end
-			end
-		end
-	end
-end]]
-
 LastSeenTbl.AddCreatureByMouseover = function(unit, seenDate)
 	if UnitGUID(unit) ~= nil then
 		local guid = UnitGUID(unit);
@@ -59,13 +31,6 @@ LastSeenTbl.AddCreatureByMouseover = function(unit, seenDate)
 					LastSeenCreaturesDB[creatureID]["unitName"] = UnitName(unit);
 				end
 			end
-			--[[if UnitClassification(unit) == "rare" or UnitClassification(unit) == "rareelite" then
-				local isInInstance = IsInInstance();
-				if not LastSeenCreaturesDB[creatureID] and UnitIsFriend(unit, L["IS_PLAYER"]) then -- To account for friendly rare encounters.
-					LastSeenCreaturesDB[creatureID] = {unitName = unitname, seen = "", player = ""};
-				end
-				RareSeen(unit, creatureID, seenDate, isInInstance);
-			end]]
 		end
 	end
 end
