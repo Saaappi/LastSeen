3568fe51407742269aab1c1b69276fdc02161d58
Alex <lightskywrites@gmail.com>
2020-05-24 04:25:42 -0500

    Commented the commands file.

--- ctl/commands.lua
+++ ctl/commands.lua
@@ -9,11 +9,11 @@ SlashCmdList["LastSeen"] = function(cmd, editbox)
 
 	if not cmd or cmd == "" then
 		addonTbl.LoadSettings(false);
-	elseif cmd == L["CMD_REMOVE"] then
+	elseif cmd == L["CMD_REMOVE"] then -- Removes an item from the items table. Accepts an ID or link.
 		addonTbl.Remove(args);
-	elseif cmd == L["CMD_SEARCH"] and args ~= "" then
+	elseif cmd == L["CMD_SEARCH"] and args ~= "" then -- Searches the items table for matches to the query. Accepts creatures, items, quests, and zones.
 		addonTbl.Search(args);
-	elseif cmd == L["CMD_VIEW"] then
+	elseif cmd == L["CMD_VIEW"] then -- When items with bad data are removed, this command allows the player to view them before they are removed, or report them.
 		if next(addonTbl.removedItems) ~= nil then
 			for k, v in pairs(addonTbl.removedItems) do
 				print(k .. ": " .. v);
@@ -21,9 +21,9 @@ SlashCmdList["LastSeen"] = function(cmd, editbox)
 		else
 			print(L["ADDON_NAME"] .. L["GENERAL_FAILURE"]);
 		end
-	elseif cmd == L["CMD_HISTORY"] then
+	elseif cmd == L["CMD_HISTORY"] then -- Allows the player to view the last 20 items they've acquired. This is persistent between sessions and characters.
 		addonTbl.GetHistory();
-	elseif cmd == L["CMD_LOOT"] then
+	elseif cmd == L["CMD_LOOT"] then -- Enables or disables a faster loot speed.
 		if addonTbl.lootFast then
 			addonTbl.lootFast = not addonTbl.lootFast; LastSeenSettingsCacheDB.lootFast = addonTbl.lootFast;
 			print(L["ADDON_NAME"] .. L["INFO_MSG_LOOT_DISABLED"]);
