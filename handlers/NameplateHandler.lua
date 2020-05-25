9099b75a4df22596ef27bf9f8aca2fc6d6176fc2
Alex <lightskywrites@gmail.com>
2020-05-24 23:34:08 -0500

    Fixed the nameplate error.

--- handlers/NameplateHandler.lua
+++ handlers/NameplateHandler.lua
@@ -29,26 +29,25 @@ addonTbl.AddCreatureByMouseover = function(unit, seenDate)
 end
 
 addonTbl.AddCreatureByNameplate = function(unit, seenDate)
-	local namePlate = C_NamePlate.GetNamePlateForUnit(unit);
-	local unitFrame = namePlate.UnitFrame;
-	if unitFrame then
-		local guid = UnitGUID(unitFrame:GetAttribute("unit"));
-		local unitname = UnitName(unitFrame:GetAttribute("unit"));
+	local guid = UnitGUID(unit);
+	local unitName = UnitName(unit);
+	if guid and unitName then
+		local entityType, _, _, _, _, creatureID, _ = strsplit("-", guid);
 	else
 		if addonTbl.mode == L["DEBUG_MODE"] then
-			print(L["ADDON_NAME"] .. L["ERROR_MSG_INVALID_UNITFRAME"] .. unit .. ".")
+			print(L["ADDON_NAME"] .. L["ERROR_MSG_INVALID_GUID_OR_UNITNAME"])
 		end
+		return;
 	end
-	local entityType, _, _, _, _, creatureID, _ = strsplit("-", guid);
 	creatureID = tonumber(creatureID);
 	if entityType == L["IS_CREATURE"] or entityType == L["IS_VEHICLE"] then
 		if not LastSeenCreaturesDB[creatureID] and not UnitIsFriend(unit, L["IS_PLAYER"]) then
-			LastSeenCreaturesDB[creatureID] = {unitName = unitname};
+			LastSeenCreaturesDB[creatureID] = {unitName = unitName};
 		elseif LastSeenCreaturesDB[creatureID] and not UnitIsFriend(unit, L["IS_PLAYER"]) then
 			if LastSeenCreaturesDB[creatureID]["seen"] == nil then
-				LastSeenCreaturesDB[creatureID] = {unitName = unitname};
+				LastSeenCreaturesDB[creatureID] = {unitName = unitName};
 			end
-			if LastSeenCreaturesDB[creatureID]["unitName"] == "Unknown" then -- LOCALIZE ME
+			if LastSeenCreaturesDB[creatureID]["unitName"] == L["IS_UNKNOWN"] then
 				LastSeenCreaturesDB[creatureID]["unitName"] = UnitName(unit);
 			end
 		end
--- locales/enUS.lua
+++ locales/enUS.lua
@@ -12,85 +12,86 @@ local LOCALE = GetLocale();
 
 if LOCALE == "enUS" then
 	-- GENERAL
-	L["ADDON_NAME"] 						= "|cff00ccff" .. addon .. "|r: ";
-	L["ADDON_NAME_SETTINGS"] 				= "|cff00ccff" .. addon .. "|r";
-	L["RELEASE"] 							= "[" .. GetAddOnMetadata(addon, "Version") .. "] ";
-	L["DATE"]								= date("%m/%d/%Y");
-	-- START AUCTION HOUSE SECTION --
-	L["AUCTION_HOUSE"] 						= "Auction House";
-	L["AUCTION_HOUSE_SOURCE"] 				= "Auction";
-	L["AUCTION_WON_SUBJECT"] 				= "Auction won:";
-	-- COMMANDS
-	L["CMD_HISTORY"] 						= "history";
-	L["CMD_LOOT"] 							= "loot";
-	L["CMD_REMOVE"] 						= "remove";
-	L["CMD_SEARCH"] 						= "search";
-	L["CMD_VIEW"] 							= "view";
-	L["SEARCH_OPTION_C"] 					= "c";
-	L["SEARCH_OPTION_I"] 					= "i";
-	L["SEARCH_OPTION_Q"] 					= "q";
-	L["SEARCH_OPTION_Z"] 					= "z";
-	-- CONSTANTS
-	L["LOOT_ITEM_PUSHED_SELF"] 				= "You receive item: ";
-	L["LOOT_ITEM_SELF"] 					= "You receive loot: ";
-	-- ERROR MESSAGES
-	L["ERROR_MSG_BAD_DATA"] 				= " item(s) removed due to missing or corrupt information.";
-	L["ERROR_MSG_BAD_REQUEST"] 				= "Bad request. Please try again.";
-	L["ERROR_MSG_CANT_ADD"] 				= "Can't add specified item.";
-	L["ERROR_MSG_CANT_COMPLETE_REQUEST"] 	= "Can't complete request: ";
-	L["ERROR_MSG_INVALID_UNITFRAME"]		= "Invalid unitframe for ";
-	L["ERROR_MSG_NO_ITEMS_FOUND"] 			= "No item(s) found.";
-	L["ERROR_MSG_UNKNOWN_SOURCE"] 			= "Unknown source for ";
-	-- INFORMATIONAL MESSAGES	
-	L["INFO_MSG_ADDON_LOAD_SUCCESSFUL"] 	= "Loaded successfully!";
-	L["INFO_MSG_ITEM_ADDED"] 				= " successfully added.";
-	L["INFO_MSG_ITEMS_FOUND"] 				= " item(s) found.";
-	L["INFO_MSG_ITEM_IGNORED"] 				= " successfully ignored.";
-	L["INFO_MSG_ITEM_REMOVED"] 				= " successfully removed.";
-	L["INFO_MSG_ITEM_UNIGNORED"] 			= " successfully unignored.";
-	L["INFO_MSG_LOOT_ENABLED"] 				= "Loot Fast mode enabled.";
-	L["INFO_MSG_LOOT_DISABLED"] 			= "Loot Fast mode disabled.";
-	L["INFO_MSG_MATCHING"] 					= " matching ";
-	-- OBJECT TYPES		
-	L["IS_CREATURE"] 						= "Creature";
-	L["IS_PLAYER"] 							= "Player";
-	L["IS_VEHICLE"] 						= "Vehicle";
-	-- MODE NAMES --		
-	L["DEBUG_MODE"] 						= "Debug";
-	L["NORMAL_MODE"] 						= "Normal";
-	L["QUIET_MODE"] 						= "Quiet";
-	-- MODE DESCRIPTIONS --		
-	L["DEBUG_MODE_DESC"] 					= "Same as normal, but variables are printed during loot operations.\n";
-	L["NORMAL_MODE_DESC"] 					= "This mode will output new and updated items.\n";
-	L["QUIET_MODE_DESC"] 					= "No output. SILENCE!\n";
+	L["ADDON_NAME"] 							= "|cff00ccff" .. addon .. "|r: ";
+	L["ADDON_NAME_SETTINGS"] 					= "|cff00ccff" .. addon .. "|r";
+	L["RELEASE"] 								= "[" .. GetAddOnMetadata(addon, "Version") .. "] ";
+	L["DATE"]									= date("%m/%d/%Y");
+	-- START AUCTION HOUSE SECTION --	
+	L["AUCTION_HOUSE"] 							= "Auction House";
+	L["AUCTION_HOUSE_SOURCE"] 					= "Auction";
+	L["AUCTION_WON_SUBJECT"] 					= "Auction won:";
+	-- COMMANDS	
+	L["CMD_HISTORY"] 							= "history";
+	L["CMD_LOOT"] 								= "loot";
+	L["CMD_REMOVE"] 							= "remove";
+	L["CMD_SEARCH"] 							= "search";
+	L["CMD_VIEW"] 								= "view";
+	L["SEARCH_OPTION_C"] 						= "c";
+	L["SEARCH_OPTION_I"] 						= "i";
+	L["SEARCH_OPTION_Q"] 						= "q";
+	L["SEARCH_OPTION_Z"] 						= "z";
+	-- CONSTANTS	
+	L["LOOT_ITEM_PUSHED_SELF"] 					= "You receive item: ";
+	L["LOOT_ITEM_SELF"] 						= "You receive loot: ";
+	-- ERROR MESSAGES	
+	L["ERROR_MSG_BAD_DATA"] 					= " item(s) removed due to missing or corrupt information.";
+	L["ERROR_MSG_BAD_REQUEST"] 					= "Bad request. Please try again.";
+	L["ERROR_MSG_CANT_ADD"] 					= "Can't add specified item.";
+	L["ERROR_MSG_CANT_COMPLETE_REQUEST"] 		= "Can't complete request: ";
+	L["ERROR_MSG_INVALID_GUID_OR_UNITNAME"]		= "Invalid GUID or unit name! Take a screenshot and report to Discord!";
+	L["ERROR_MSG_NO_ITEMS_FOUND"] 				= "No item(s) found.";
+	L["ERROR_MSG_UNKNOWN_SOURCE"] 				= "Unknown source for ";
+	-- INFORMATIONAL MESSAGES		
+	L["INFO_MSG_ADDON_LOAD_SUCCESSFUL"] 		= "Loaded successfully!";
+	L["INFO_MSG_ITEM_ADDED"] 					= " successfully added.";
+	L["INFO_MSG_ITEMS_FOUND"] 					= " item(s) found.";
+	L["INFO_MSG_ITEM_IGNORED"] 					= " successfully ignored.";
+	L["INFO_MSG_ITEM_REMOVED"] 					= " successfully removed.";
+	L["INFO_MSG_ITEM_UNIGNORED"] 				= " successfully unignored.";
+	L["INFO_MSG_LOOT_ENABLED"] 					= "Loot Fast mode enabled.";
+	L["INFO_MSG_LOOT_DISABLED"] 				= "Loot Fast mode disabled.";
+	L["INFO_MSG_MATCHING"] 						= " matching ";
+	-- OBJECT TYPES			
+	L["IS_CREATURE"] 							= "Creature";
+	L["IS_PLAYER"] 								= "Player";
+	L["IS_VEHICLE"] 							= "Vehicle";
+	L["IS_UNKNOWN"]								= "Unknown";
+	-- MODE NAMES --			
+	L["DEBUG_MODE"] 							= "Debug";
+	L["NORMAL_MODE"] 							= "Normal";
+	L["QUIET_MODE"] 							= "Quiet";
+	-- MODE DESCRIPTIONS --			
+	L["DEBUG_MODE_DESC"] 						= "Same as normal, but variables are printed during loot operations.\n";
+	L["NORMAL_MODE_DESC"] 						= "This mode will output new and updated items.\n";
+	L["QUIET_MODE_DESC"] 						= "No output. SILENCE!\n";
 	-- OTHER
 	L["SPELL_NAMES"] = {
 		{
-			["spellName"] = "Collecting",
-		},
-		{
-			["spellName"] = "Fishing",
-		},
-		{
-			["spellName"] = "Frisking Toga",
-		},
-		{
-			["spellName"] = "Herb Gathering",
-		},
-		{
-			["spellName"] = "Looting",
-		},
-		{
-			["spellName"] = "Mining",
-		},
-		{
-			["spellName"] = "Opening",
-		},
-		{
-			["spellName"] = "Skinning",
-		},
-		{
-			["spellName"] = "Survey",
+			["spellName"] 						= "Collecting",
+		},						
+		{						
+			["spellName"] 						= "Fishing",
+		},						
+		{						
+			["spellName"] 						= "Frisking Toga",
+		},						
+		{						
+			["spellName"] 						= "Herb Gathering",
+		},						
+		{						
+			["spellName"] 						= "Looting",
+		},						
+		{						
+			["spellName"] 						= "Mining",
+		},						
+		{						
+			["spellName"] 						= "Opening",
+		},						
+		{						
+			["spellName"] 						= "Skinning",
+		},						
+		{						
+			["spellName"] 						= "Survey",
 		},
 	};
 return end;
