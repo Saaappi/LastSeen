2f9bc44252a0deb63662e93f3efdf6e428492255
Alex <lightskywrites@gmail.com>
2020-06-04 20:12:15 -0500

    Mark of Honor is ignored once again.

--- DB/DB_IgnoredItems.lua
+++ DB/DB_IgnoredItems.lua
@@ -3,12 +3,13 @@ local L = addonTbl.L;
 
 -- TODO: Reconsider the approach to ignoring specific items.
 local ignoredItems = {
-	[21218] = "Blue Qiraji Resonating Crystal",
-	[141605] = "Flight Master's Whistle",
-    [21323] = "Green Qiraji Resonating Crystal",
-	[11736] = "Libram of Resilience",
-    [21321] = "Red Qiraji Resonating Crystal",
-    [21324] = "Yellow Qiraji Resonating Crystal",
+	[21218] 		= "Blue Qiraji Resonating Crystal",
+	[141605] 		= "Flight Master's Whistle",
+    [21323] 		= "Green Qiraji Resonating Crystal",
+	[11736] 		= "Libram of Resilience",
+	[137642]		= "Mark of Honor",
+    [21321] 		= "Red Qiraji Resonating Crystal",
+    [21324] 		= "Yellow Qiraji Resonating Crystal",
 };
 
 addonTbl.ignoredItems = ignoredItems;
