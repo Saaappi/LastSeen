c536136fa86a924b2eedac8c08540dc2db177526
Alex <lightskywrites@gmail.com>
2020-06-12 17:17:04 -0500

    Added the ITEM_DATA_LOAD_RESULT event. Reorganized the events in the Main file to match the order in the DB_Events file. Added comments to the MERCHANT_CLOSED/SHOW events.

--- main.lua
+++ main.lua
@@ -70,6 +70,151 @@ end
 -- Synopsis: When executed, after 4 seconds, clear or reset the variables.
 
 frame:SetScript("OnEvent", function(self, event, ...)
+
+	if event == "CHAT_MSG_LOOT" then
+		if addonTbl.encounterID then return end;
+		if LastSeenQuestsDB[addonTbl.questID] then return end;
+		if isMerchantFrameOpen then return end;
+		
+		local text, name = ...; name = string.match(name, "(.*)-");
+		if name == playerName then
+			text = string.match(text, L["LOOT_ITEM_PUSHED_SELF"] .. "(.*).");
+			if text then
+				local itemID, itemType, itemSubType, itemEquipLoc, itemIcon = GetItemInfoInstant(text);
+				itemName = (GetItemInfo(text));
+				itemRarity = select(3, GetItemInfo(text));
+				
+				if LastSeenItemsDB[itemID] then
+					addonTbl.AddItem(itemID, text, itemName, itemRarity, itemType, itemSubType, itemEquipLoc, itemIcon, L["DATE"], addonTbl.currentMap, "Miscellaneous", L["INFO_MSG_MISCELLANEOUS"], "Update");
+				else
+					addonTbl.AddItem(itemID, text, itemName, itemRarity, itemType, itemSubType, itemEquipLoc, itemIcon, L["DATE"], addonTbl.currentMap, "Miscellaneous", L["INFO_MSG_MISCELLANEOUS"], "New");
+				end
+			end
+		end
+	end
+	-- Synopsis: When the player is rewarded an item, usually when the player didn't loot anything by action, track it using the source of said item. Typically, we would see this occur in many cases,
+	-- but we really only care about acquisition via creatures like unlootable world bosses.
+	
+	if event == "ENCOUNTER_START" then
+		local _, encounterName = ...;
+		addonTbl.encounterID = addonTbl.GetTableKeyFromValue(LastSeenEncountersDB, encounterName);
+	end
+	-- Synopsis: Used to capture the encounter ID for the current instance encounter.
+	
+	if event == "INSTANCE_GROUP_SIZE_CHANGED" or "ZONE_CHANGED_NEW_AREA" then
+		local realZoneText = GetRealZoneText();
+		
+		if IsPlayerInCombat() then
+			while isPlayerInCombat do
+				C_Timer.After(0, function() C_Timer.After(3, function() IsPlayerInCombat() end); end);
+			end
+		end
+		
+		if realZoneText then
+			if addonTbl.currentMap ~= realZoneText then
+				addonTbl.GetCurrentMap();
+			end
+		else
+			C_Timer.After(0, function() C_Timer.After(3, function() addonTbl.GetCurrentMap() end); end);
+		end
+		
+		if not (realZoneText == addonTbl.currentMap) then
+			addonTbl.currentMap = realZoneText;
+		end
+	end
+	-- Synopsis: Get the player's map when they change zones or enter instances.
+	
+	if event == "ITEM_DATA_LOAD_RESULT" then
+	
+	end
+	-- Synopsis: Used to capture loot obtained from Island Expeditions.
+	
+	if event == "LOOT_CLOSED" then
+		EmptyVariables();
+	end
+	-- Synopsis: When the loot window is closed, call the EmptyVariables function.
+	
+	if event == "LOOT_OPENED" then
+		plsEmptyVariables = true;
+		local lootSlots = GetNumLootItems(); addonTbl.lootSlots = lootSlots;
+		if lootSlots < 1 then return end;
+		
+		if addonTbl.lootFast then
+			if (GetTime() - epoch) >= delay then
+				for slot = lootSlots, 1, -1 do
+					addonTbl.GetItemInfo(GetLootSlotLink(slot), slot);
+					if addonTbl.doNotLoot == false then
+						LootSlot(slot);
+					end
+				end
+			end
+			epoch = GetTime();
+		else
+			for slot = lootSlots, 1, -1 do
+				addonTbl.GetItemInfo(GetLootSlotLink(slot), slot);
+			end
+		end
+	end
+	--[[
+		Synopsis: Fires when the loot window is opened in MOST situations.
+		Use Case(s):
+			- Creatures
+			- Objects
+	]]
+	
+	if event == "MAIL_INBOX_UPDATE" then
+		local mailItems = GetInboxNumItems();
+		if mailItems > 0 then
+			for i = 1, mailItems do
+				local _, _, sender, subject = GetInboxHeaderInfo(i);
+				if sender == L["AUCTION_HOUSE"] then
+					if strfind(subject, L["AUCTION_WON_SUBJECT"]) then
+						for j = 1, ATTACHMENTS_MAX_RECEIVE do 
+							itemLink = GetInboxItemLink(i, j);
+							if itemLink then
+								itemID, itemType, itemSubType, itemEquipLoc, itemIcon = GetItemInfoInstant(itemLink);
+								itemName = (GetItemInfo(itemLink));
+								itemRarity = select(3, GetItemInfo(itemLink));
+								if itemRarity >= addonTbl.rarity then
+									if addonTbl.Contains(addonTbl.ignoredItems, itemID, nil, nil) then return end;
+									if LastSeenItemsDB[itemID] then
+										addonTbl.AddItem(itemID, itemLink, itemName, itemRarity, itemType, itemSubType, itemEquipLoc, itemIcon, L["DATE"], addonTbl.currentMap, "Auction", L["AUCTION_HOUSE"], "Update");
+									else
+										addonTbl.AddItem(itemID, itemLink, itemName, itemRarity, itemType, itemSubType, itemEquipLoc, itemIcon, L["DATE"], addonTbl.currentMap, "Auction", L["AUCTION_HOUSE"], "New");
+									end
+								end
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+	-- Synopsis: Used to capture items bought from the Auction House.
+	-- ATTACHMENTS_MAX_RECEIVE: 16
+	
+	if event == "MERCHANT_CLOSED" then isMerchantFrameOpen = false end;
+	
+	if event == "MERCHANT_SHOW" then isMerchantFrameOpen = true end;
+	-- Synopsis: The merchant events prevent items bought from vendors from adding to the items table.
+	
+	if event == "MODIFIER_STATE_CHANGED" then
+		local key, down = ...;
+		if down == 1 then
+			if key == "LSHIFT" or key == "RSHIFT" then
+				addonTbl.doNotLoot = true;
+			end
+		else
+			addonTbl.doNotLoot = false;
+		end
+	end
+	-- Synopsis: Allows players to prevent the game from looting items like lockboxes.
+	
+	if event == "NAME_PLATE_UNIT_ADDED" then
+		local unit = ...;
+		addonTbl.AddCreatureByNameplate(unit, L["DATE"]);
+	end
+	-- Synopsis: When a nameplate appears on the screen, pass the GUID down the pipeline so it can be scanned for the creature's name.
 	
 	if event == "PLAYER_LOGIN" and addonTbl.isLastSeenLoaded then
 		if LastSeenMapsDB == nil then LastSeenMapsDB = InitializeTable(LastSeenMapsDB) end;
@@ -123,117 +268,12 @@ frame:SetScript("OnEvent", function(self, event, ...)
 			badDataItemCount = 0;
 		end
 	end
-	if event == "ZONE_CHANGED_NEW_AREA" or "INSTANCE_GROUP_SIZE_CHANGED" then
-		local realZoneText = GetRealZoneText();
-		
-		if IsPlayerInCombat() then
-			while isPlayerInCombat do
-				C_Timer.After(0, function() C_Timer.After(3, function() IsPlayerInCombat() end); end);
-			end
-		end
-		
-		if realZoneText then
-			if addonTbl.currentMap ~= realZoneText then
-				addonTbl.GetCurrentMap();
-			end
-		else
-			C_Timer.After(0, function() C_Timer.After(3, function() addonTbl.GetCurrentMap() end); end);
-		end
-		
-		if not (realZoneText == addonTbl.currentMap) then
-			addonTbl.currentMap = realZoneText;
-		end
+	
+	if event == "PLAYER_LOGOUT" then
+		addonTbl.itemsToSource = {}; -- Items looted from creatures are stored here and compared against the creature table to find where they dropped from, they are stored here until the below scenario occurs.
+		addonTbl.removedItems = {}; -- When items with 'bad' data are removed, they are stored here until the below scenario occurs.
 	end
-	-- Synopsis: Get the player's map when they change zones or enter instances.
-	
-	if event == "MODIFIER_STATE_CHANGED" then
-		local key, down = ...;
-		if down == 1 then
-			if key == "LSHIFT" or key == "RSHIFT" then
-				addonTbl.doNotLoot = true;
-			end
-		else
-			addonTbl.doNotLoot = false;
-		end
-	end
-	-- Synopsis: Allows players to prevent the game from looting items like lockboxes.
-	
-	if event == "UNIT_SPELLCAST_SENT" then
-		local unit, target, _, spellID = ...; local spellName = GetSpellInfo(spellID);
-		if unit == string.lower(L["IS_PLAYER"]) then
-			if addonTbl.Contains(L["SPELL_NAMES"], nil, "spellName", spellName) then
-				addonTbl.target = target;
-			end
-		end
-	end
-	-- Synopsis: Used to capture the name of an object that the player loots.
-	
-	if event == "ENCOUNTER_START" then
-		local _, encounterName = ...;
-		addonTbl.encounterID = addonTbl.GetTableKeyFromValue(LastSeenEncountersDB, encounterName);
-	end
-	-- Synopsis: Used to capture the encounter ID for the current instance encounter.
-	
-	if event == "LOOT_OPENED" then
-		plsEmptyVariables = true;
-		local lootSlots = GetNumLootItems(); addonTbl.lootSlots = lootSlots;
-		if lootSlots < 1 then return end;
-		
-		if addonTbl.lootFast then
-			if (GetTime() - epoch) >= delay then
-				for slot = lootSlots, 1, -1 do
-					addonTbl.GetItemInfo(GetLootSlotLink(slot), slot);
-					if addonTbl.doNotLoot == false then
-						LootSlot(slot);
-					end
-				end
-			end
-			epoch = GetTime();
-		else
-			for slot = lootSlots, 1, -1 do
-				addonTbl.GetItemInfo(GetLootSlotLink(slot), slot);
-			end
-		end
-	end
-	--[[
-		Synopsis: Fires when the loot window is opened in MOST situations.
-		Use Case(s):
-			- Creatures
-			- Objects
-	]]
-	
-	if event == "LOOT_CLOSED" then
-		EmptyVariables();
-	end
-	-- Synopsis: When the loot window is closed, call the EmptyVariables function.
-	
-	if event == "MERCHANT_SHOW" then isMerchantFrameOpen = true end;
-	
-	if event == "MERCHANT_CLOSED" then isMerchantFrameOpen = false end;
-	
-	if event == "CHAT_MSG_LOOT" then
-		if addonTbl.encounterID then return end;
-		if LastSeenQuestsDB[addonTbl.questID] then return end;
-		if isMerchantFrameOpen then return end;
-		
-		local text, name = ...; name = string.match(name, "(.*)-");
-		if name == playerName then
-			text = string.match(text, L["LOOT_ITEM_PUSHED_SELF"] .. "(.*).");
-			if text then
-				local itemID, itemType, itemSubType, itemEquipLoc, itemIcon = GetItemInfoInstant(text);
-				itemName = (GetItemInfo(text));
-				itemRarity = select(3, GetItemInfo(text));
-				
-				if LastSeenItemsDB[itemID] then
-					addonTbl.AddItem(itemID, text, itemName, itemRarity, itemType, itemSubType, itemEquipLoc, itemIcon, L["DATE"], addonTbl.currentMap, "Miscellaneous", L["INFO_MSG_MISCELLANEOUS"], "Update");
-				else
-					addonTbl.AddItem(itemID, text, itemName, itemRarity, itemType, itemSubType, itemEquipLoc, itemIcon, L["DATE"], addonTbl.currentMap, "Miscellaneous", L["INFO_MSG_MISCELLANEOUS"], "New");
-				end
-			end
-		end
-	end
-	-- Synopsis: When the player is rewarded an item, usually when the player didn't loot anything by action, track it using the source of said item. Typically, we would see this occur in many cases,
-	-- but we really only care about acquisition via creatures like unlootable world bosses.
+	-- Synopsis: Clear out data that's no longer needed when the player logs off or reloads their user interface.
 	
 	if event == "QUEST_ACCEPTED" then
 		local questIndex = ...; addonTbl.GetQuestInfo(questIndex);
@@ -273,53 +313,20 @@ frame:SetScript("OnEvent", function(self, event, ...)
 	end
 	-- Synopsis: Fires whenever a player completes a quest and receives a quest reward. This tracks the reward by the name of the quest.
 	
-	if event == "MAIL_INBOX_UPDATE" then
-		local mailItems = GetInboxNumItems();
-		if mailItems > 0 then
-			for i = 1, mailItems do
-				local _, _, sender, subject = GetInboxHeaderInfo(i);
-				if sender == L["AUCTION_HOUSE"] then
-					if strfind(subject, L["AUCTION_WON_SUBJECT"]) then
-						for j = 1, ATTACHMENTS_MAX_RECEIVE do 
-							itemLink = GetInboxItemLink(i, j);
-							if itemLink then
-								itemID, itemType, itemSubType, itemEquipLoc, itemIcon = GetItemInfoInstant(itemLink);
-								itemName = (GetItemInfo(itemLink));
-								itemRarity = select(3, GetItemInfo(itemLink));
-								if itemRarity >= addonTbl.rarity then
-									if addonTbl.Contains(addonTbl.ignoredItems, itemID, nil, nil) then return end;
-									if LastSeenItemsDB[itemID] then
-										addonTbl.AddItem(itemID, itemLink, itemName, itemRarity, itemType, itemSubType, itemEquipLoc, itemIcon, L["DATE"], addonTbl.currentMap, "Auction", L["AUCTION_HOUSE"], "Update");
-									else
-										addonTbl.AddItem(itemID, itemLink, itemName, itemRarity, itemType, itemSubType, itemEquipLoc, itemIcon, L["DATE"], addonTbl.currentMap, "Auction", L["AUCTION_HOUSE"], "New");
-									end
-								end
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-	-- Synopsis: Used to capture items bought from the Auction House.
-	-- ATTACHMENTS_MAX_RECEIVE: 16
-
-	if event == "NAME_PLATE_UNIT_ADDED" then
-		local unit = ...;
-		addonTbl.AddCreatureByNameplate(unit, L["DATE"]);
-	end
-	-- Synopsis: When a nameplate appears on the screen, pass the GUID down the pipeline so it can be scanned for the creature's name.
-	
 	if event == "UPDATE_MOUSEOVER_UNIT" then
 		addonTbl.AddCreatureByMouseover("mouseover", L["DATE"]);
 	end
 	-- Synopsis: When the player hovers over a target without a nameplate, or the player doesn't use nameplates, send the GUID down the pipeline so it can be scanned for the creature's name.
 	
-	if event == "PLAYER_LOGOUT" then
-		addonTbl.itemsToSource = {}; -- Items looted from creatures are stored here and compared against the creature table to find where they dropped from, they are stored here until the below scenario occurs.
-		addonTbl.removedItems = {}; -- When items with 'bad' data are removed, they are stored here until the below scenario occurs.
+	if event == "UNIT_SPELLCAST_SENT" then
+		local unit, target, _, spellID = ...; local spellName = GetSpellInfo(spellID);
+		if unit == string.lower(L["IS_PLAYER"]) then
+			if addonTbl.Contains(L["SPELL_NAMES"], nil, "spellName", spellName) then
+				addonTbl.target = target;
+			end
+		end
 	end
-	-- Synopsis: Clear out data that's no longer needed when the player logs off or reloads their user interface.
+	-- Synopsis: Used to capture the name of an object that the player loots.
 end);
 
 GameTooltip:HookScript("OnTooltipSetItem", addonTbl.OnTooltipSetItem);
