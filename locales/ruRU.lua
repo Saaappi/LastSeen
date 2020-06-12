4437805e572278cfb6a1d62e6cda9b4cd8b4ff3e
Alex <lightskywrites@gmail.com>
2020-06-12 17:34:48 -0500

    Updated the ruRU locale file.

--- locales/ruRU.lua
+++ locales/ruRU.lua
@@ -10,15 +10,7 @@ local LOCALE = GetLocale();
 
 if LOCALE == "ruRU" then -- Russian
 	addonTbl.L = L;
-
-	-- GENERAL
-	L["ADDON_NAME"] 							= "|cff00ccff" .. addon .. "|r: ";
-	L["ADDON_NAME_SETTINGS"] 					= "|cff00ccff" .. addon .. "|r";
-	L["RELEASE"] 								= "[" .. GetAddOnMetadata(addon, "Version") .. "] ";
-	L["DATE"]									= date("%m/%d/%Y");
-	-- START AUCTION HOUSE SECTION --	
-	L["AUCTION_HOUSE"] 							= "Аукционный дом";
-	L["AUCTION_WON_SUBJECT"] 					= "Вы выиграли торги:";
+	
 	-- COMMANDS
 	L["CMD_DISCORD"]							= "диссонанс";
 	L["CMD_HISTORY"] 							= "история";
@@ -32,10 +24,8 @@ if LOCALE == "ruRU" then -- Russian
 	L["SEARCH_OPTION_I"] 						= "i";
 	L["SEARCH_OPTION_Q"] 						= "q";
 	L["SEARCH_OPTION_Z"] 						= "z";
-	-- CONSTANTS	
-	L["LOOT_ITEM_PUSHED_SELF"] 					= "Вы получаете предмет: ";
-	L["LOOT_ITEM_SELF"] 						= "Ваша добыча: ";
-	-- ERROR MESSAGES	
+	
+	-- ERROR MESSAGES
 	L["ERROR_MSG_BAD_DATA"] 					= " пункт(ы), удаленный(ые) из-за отсутствующей или поврежденной информации.";
 	L["ERROR_MSG_BAD_REQUEST"] 					= "Плохая просьба. Пожалуйста, попробуйте еще раз.";
 	L["ERROR_MSG_CANT_ADD"] 					= "Не может добавить указанный пункт.";
@@ -43,7 +33,21 @@ if LOCALE == "ruRU" then -- Russian
 	L["ERROR_MSG_INVALID_GUID_OR_UNITNAME"]		= "Недействительный GUID или имя устройства! Сделайте снимок экрана и доложите о результатах Раздора!";
 	L["ERROR_MSG_NO_ITEMS_FOUND"] 				= "Ничего не найдено.";
 	L["ERROR_MSG_UNKNOWN_SOURCE"] 				= " был ограблен из неизвестного источника. Его источник был установлен как \"Разное\".";
-	-- INFORMATIONAL MESSAGES		
+
+	-- GENERAL
+	L["ADDON_NAME"] 							= "|cff00ccff" .. addon .. "|r: ";
+	L["ADDON_NAME_SETTINGS"] 					= "|cff00ccff" .. addon .. "|r";
+	L["RELEASE"] 								= "[" .. GetAddOnMetadata(addon, "Version") .. "] ";
+	L["DATE"]									= date("%m/%d/%Y");
+	
+	-- GLOBAL STRINGS
+	L["AUCTION_HOUSE"] 							= "Аукционный дом";
+	L["AUCTION_WON_SUBJECT"] 					= "Вы выиграли торги:";
+	L["ISLAND_EXPEDITIONS"]						= "Островные экспедиции";
+	L["LOOT_ITEM_PUSHED_SELF"] 					= "Вы получаете предмет: ";
+	L["LOOT_ITEM_SELF"] 						= "Ваша добыча: ";
+	
+	-- INFORMATIONAL MESSAGES
 	L["INFO_MSG_ADDON_LOAD_SUCCESSFUL"] 		= "Заряжено успешно!";
 	L["INFO_MSG_IGNORED_ITEM"] 					= "Этот пункт автоматически игнорируется.";
 	L["INFO_MSG_ITEM_REMOVED"] 					= " успешно удалено.";
@@ -51,19 +55,22 @@ if LOCALE == "ruRU" then -- Russian
 	L["INFO_MSG_LOOT_DISABLED"] 				= "Режим Loot Fast отключен.";
 	L["INFO_MSG_MISCELLANEOUS"]					= "Разное";
 	L["INFO_MSG_RESULTS"]						= " результат(ы)";
-	-- OBJECT TYPES			
+	
+	-- MODE NAMES
+	L["DEBUG_MODE"] 							= "Отладка";
+	L["NORMAL_MODE"] 							= "Нормальный";
+	L["QUIET_MODE"] 							= "Тихий";
+	-- MODE DESCRIPTIONS
+	L["DEBUG_MODE_DESC"] 						= "Нормальный режим с переменным выходом.\n";
+	L["NORMAL_MODE_DESC"] 						= "Показывает новые и обновленные пункты.\n";
+	L["QUIET_MODE_DESC"] 						= "Выхода нет!\n";
+	
+	-- OBJECT TYPES
 	L["IS_CREATURE"] 							= "Существо";
 	L["IS_PLAYER"] 								= "Игрок";
 	L["IS_VEHICLE"] 							= "Автомобиль";
 	L["IS_UNKNOWN"]								= "Неизвестно";
-	-- MODE NAMES --			
-	L["DEBUG_MODE"] 							= "Отладка";
-	L["NORMAL_MODE"] 							= "Нормальный";
-	L["QUIET_MODE"] 							= "Тихий";
-	-- MODE DESCRIPTIONS --			
-	L["DEBUG_MODE_DESC"] 						= "Нормальный режим с переменным выходом.\n";
-	L["NORMAL_MODE_DESC"] 						= "Показывает новые и обновленные пункты.\n";
-	L["QUIET_MODE_DESC"] 						= "Выхода нет!\n";
+	
 	-- OTHER
 	L["SPELL_NAMES"] = {
 		{
