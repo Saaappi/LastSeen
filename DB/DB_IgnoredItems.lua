d82848f55576b00388336b9b3c58f7c5cd05e938
Alex <lightskywrites@gmail.com>
2020-06-12 18:55:43 -0500

    All seafarer doubloon containers are now ignored. Reorganized the ignored items table so lookups are a tad bit faster.

--- DB/DB_IgnoredItems.lua
+++ DB/DB_IgnoredItems.lua
@@ -3,14 +3,16 @@ local L = addonTbl.L;
 
 -- TODO: Reconsider the approach to ignoring specific items.
 local ignoredItems = {
-	[21218] 		= "Blue Qiraji Resonating Crystal",
-	[141605] 		= "Flight Master's Whistle",
-    [21323] 		= "Green Qiraji Resonating Crystal",
 	[11736] 		= "Libram of Resilience",
-	[137642]		= "Mark of Honor",
+	[21218] 		= "Blue Qiraji Resonating Crystal",
     [21321] 		= "Red Qiraji Resonating Crystal",
-	[163611]		= "Seafarer's Coin Pouch",
+    [21323] 		= "Green Qiraji Resonating Crystal",
     [21324] 		= "Yellow Qiraji Resonating Crystal",
+	[137642]		= "Mark of Honor",
+	[141605] 		= "Flight Master's Whistle",
+	[163611]		= "Seafarer's Coin Pouch",
+	[163612]		= "Wayfinder's Satchel",
+	[163613]		= "Sack of Plunder",
 };
 
 addonTbl.ignoredItems = ignoredItems;
