77beb1f4abdb0f979148565985997ad109ffb56d
Alex <lightskywrites@gmail.com>
2020-06-12 17:20:34 -0500

    Reorganized the enUS file.

--- locales/enUS.lua
+++ locales/enUS.lua
@@ -10,15 +10,7 @@ local LOCALE = GetLocale();
 
 if LOCALE == "enUS" then -- US English
 	addonTbl.L = L;
-
-	-- GENERAL
-	L["ADDON_NAME"] 							= "|cff00ccff" .. addon .. "|r: ";
-	L["ADDON_NAME_SETTINGS"] 					= "|cff00ccff" .. addon .. "|r";
-	L["RELEASE"] 								= "[" .. GetAddOnMetadata(addon, "Version") .. "] ";
-	L["DATE"]									= date("%m/%d/%Y");
-	-- START AUCTION HOUSE SECTION --	
-	L["AUCTION_HOUSE"] 							= "Auction House";
-	L["AUCTION_WON_SUBJECT"] 					= "Auction won:";
+	
 	-- COMMANDS
 	L["CMD_DISCORD"]							= "discord";
 	L["CMD_HISTORY"] 							= "history";
@@ -32,9 +24,7 @@ if LOCALE == "enUS" then -- US English
 	L["SEARCH_OPTION_I"] 						= "i";
 	L["SEARCH_OPTION_Q"] 						= "q";
 	L["SEARCH_OPTION_Z"] 						= "z";
-	-- CONSTANTS	
-	L["LOOT_ITEM_PUSHED_SELF"] 					= "You receive item: ";
-	L["LOOT_ITEM_SELF"] 						= "You receive loot: ";
+	
 	-- ERROR MESSAGES	
 	L["ERROR_MSG_BAD_DATA"] 					= " item(s) removed due to missing or corrupt information.";
 	L["ERROR_MSG_BAD_REQUEST"] 					= "Bad request. Please try again.";
@@ -43,6 +33,19 @@ if LOCALE == "enUS" then -- US English
 	L["ERROR_MSG_INVALID_GUID_OR_UNITNAME"]		= "Invalid GUID or unit name! Take a screenshot and report to Discord!";
 	L["ERROR_MSG_NO_ITEMS_FOUND"] 				= "No item(s) found.";
 	L["ERROR_MSG_UNKNOWN_SOURCE"] 				= " was looted from an unknown source. Its source has been set as Miscellaneous.";
+	
+	-- GENERAL
+	L["ADDON_NAME"] 							= "|cff00ccff" .. addon .. "|r: ";
+	L["ADDON_NAME_SETTINGS"] 					= "|cff00ccff" .. addon .. "|r";
+	L["RELEASE"] 								= "[" .. GetAddOnMetadata(addon, "Version") .. "] ";
+	L["DATE"]									= date("%m/%d/%Y");
+	
+	-- GLOBAL STRINGS
+	L["AUCTION_HOUSE"]							= "Auction House";
+	L["AUCTION_WON_SUBJECT"]					= "Auction won:";
+	L["LOOT_ITEM_PUSHED_SELF"] 					= "You receive item: ";
+	L["LOOT_ITEM_SELF"] 						= "You receive loot: ";
+	
 	-- INFORMATIONAL MESSAGES		
 	L["INFO_MSG_ADDON_LOAD_SUCCESSFUL"] 		= "Loaded successfully!";
 	L["INFO_MSG_IGNORED_ITEM"] 					= "This item is automatically ignored.";
@@ -51,19 +54,23 @@ if LOCALE == "enUS" then -- US English
 	L["INFO_MSG_LOOT_DISABLED"] 				= "Loot Fast mode disabled.";
 	L["INFO_MSG_MISCELLANEOUS"]					= "Miscellaneous";
 	L["INFO_MSG_RESULTS"]						= " result(s)";
+	
+	-- MODE NAMES --			
+	L["DEBUG_MODE"] 							= "Debug";
+	L["NORMAL_MODE"] 							= "Normal";
+	L["QUIET_MODE"] 							= "Quiet";
+	
+	-- MODE DESCRIPTIONS --			
+	L["DEBUG_MODE_DESC"] 						= "Normal mode with variable output.\n";
+	L["NORMAL_MODE_DESC"] 						= "Shows new and updated items.\n";
+	L["QUIET_MODE_DESC"] 						= "No output!\n";
+	
 	-- OBJECT TYPES			
 	L["IS_CREATURE"] 							= "Creature";
 	L["IS_PLAYER"] 								= "Player";
 	L["IS_VEHICLE"] 							= "Vehicle";
 	L["IS_UNKNOWN"]								= "Unknown";
-	-- MODE NAMES --			
-	L["DEBUG_MODE"] 							= "Debug";
-	L["NORMAL_MODE"] 							= "Normal";
-	L["QUIET_MODE"] 							= "Quiet";
-	-- MODE DESCRIPTIONS --			
-	L["DEBUG_MODE_DESC"] 						= "Normal mode with variable output.\n";
-	L["NORMAL_MODE_DESC"] 						= "Shows new and updated items.\n";
-	L["QUIET_MODE_DESC"] 						= "No output!\n";
+	
 	-- OTHER
 	L["SPELL_NAMES"] = {
 		{
