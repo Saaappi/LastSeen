d280bb4d00bc505320f95011fc67174d544b8f19
Alex <lightskywrites@gmail.com>
2020-06-14 02:11:59 -0500

    Debug (testing) print added for addonTbl.target.

--- main.lua
+++ main.lua
@@ -341,6 +341,7 @@ frame:SetScript("OnEvent", function(self, event, ...)
 		if unit == string.lower(L["IS_PLAYER"]) then
 			if addonTbl.Contains(L["SPELL_NAMES"], nil, "spellName", spellName) then
 				addonTbl.target = target;
+				print(addonTbl.target);
 			end
 		end
 	end
