c66827e46ca8ac8bf1a02913a7bcc6d72cabf264
Alex <lightskywrites@gmail.com>
2020-06-17 03:25:21 -0500

    The player's search query is returned if no results are found.

--- handlers/FunctionHandler.lua
+++ handlers/FunctionHandler.lua
@@ -101,7 +101,7 @@ addonTbl.Search = function(query)
 			end
 		end
 		if itemsFound == 0 then
-			print(L["ADDON_NAME"] .. L["ERROR_MSG_NO_ITEMS_FOUND"]);
+			print(L["ADDON_NAME"] .. L["ERROR_MSG_NO_ITEMS_FOUND"] .. " (" .. query .. ")");
 		else
 			print(L["ADDON_NAME"] .. itemsFound .. L["INFO_MSG_RESULTS"]);
 		end
