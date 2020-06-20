77f199b4fe9d9afca2de502bec8d4a0b1fef2f76
Alex <lightskywrites@gmail.com>
2020-06-20 15:57:16 -0500

    Added the show sources button. Still needs a proper name and description. Changed the ownership of the tooltip within the settings frame.

--- Settings.lua
+++ Settings.lua
@@ -31,6 +31,10 @@ local function GetOptions(arg)
 			LastSeenSettingsCacheDB[arg] = true; addonTbl[arg] = LastSeenSettingsCacheDB[arg];
 			return addonTbl[arg];
 		end
+		if arg == "showSources" then
+			LastSeenSettingsCacheDB[arg] = false; addonTbl[arg] = LastSeenSettingsCacheDB[arg];
+			return addonTbl[arg];
+		end
 	end
 end
 -- Synopsis: When the addon is loaded into memory after login, the addon will ask the cache for the last known
@@ -56,7 +60,7 @@ local function SettingsMenu_OnShow()
 		settingsFrame:RegisterForDrag("LeftButton");
 		settingsFrame:SetScript("OnDragStart", settingsFrame.StartMoving);
 		settingsFrame:SetScript("OnDragStop", settingsFrame.StopMovingOrSizing);
-		settingsFrame:SetSize(200, 100);
+		settingsFrame:SetSize(200, 125);
 		settingsFrame:ClearAllPoints();
 		settingsFrame:SetPoint("CENTER", WorldFrame, "CENTER");
 	end
@@ -72,7 +76,7 @@ local function SettingsMenu_OnShow()
 	
 	if not settingsFrame.modeDropDown then
 		settingsFrame.modeDropDown = CreateFrame("Frame", "addonModeDropDown", settingsFrame, "UIDropDownMenuTemplate");
-		settingsFrame.modeDropDown:SetPoint("CENTER", settingsFrame, "CENTER");
+		settingsFrame.modeDropDown:SetPoint("CENTER", settingsFrame, "CENTER", 0, 15);
 		settingsFrame.modeDropDown:SetSize(175, 30);
 		settingsFrame.modeDropDown.initialize = function(self, level)
 			modeList = UIDropDownMenu_CreateInfo();
@@ -98,20 +102,43 @@ local function SettingsMenu_OnShow()
 	if not settingsFrame.itemCount then
 		settingsFrame.itemCount = settingsFrame:CreateFontString(nil, "OVERLAY");
 		settingsFrame.itemCount:SetFontObject("GameFontHighlight");
-		settingsFrame.itemCount:SetPoint("CENTER", settingsFrame, "CENTER", 0, -25);
+		settingsFrame.itemCount:SetPoint("CENTER", settingsFrame, "CENTER", 0, -10);
 		settingsFrame.itemCount:SetText(addonTbl.GetCount(LastSeenItemsDB));
 	end
 	-- Synopsis: This is a count of how many items the player has seen. It counts from the cached database so
 	-- players will need to reload to see items that were just looted.
+	
+	if not settingsFrame.showSourcesButton then
+		settingsFrame.showSourcesButton = CreateFrame("CheckButton", "LastSeenShowSourcesButton", settingsFrame, "UICheckButtonTemplate");
+		settingsFrame.showSourcesButton:SetPoint("CENTER", settingsFrame, "CENTER", -60, -35);
+		settingsFrame.showSourcesButton.text:SetText("LOL");
+	end
+	settingsFrame.showSourcesButton:SetScript("OnClick", function(self, event, arg1)
+		if self:GetChecked() then
+			addonTbl.showSources = true;
+			LastSeenSettingsCacheDB.showSources = true;
+		else
+			addonTbl.showSources = false;
+			LastSeenSettingsCacheDB.showSources = false;
+		end
+	end);
+	-- Synopsis: Tells the addon whether or not to show some of the sources an item was seen from.
+	
+	if LastSeenSettingsCacheDB.showSources then
+		settingsFrame.showSourcesButton:SetChecked(true);
+		addonTbl.showSources = true;
+	else
+		settingsFrame.showSourcesButton:SetChecked(false);
+		addonTbl.showSources = false;
+	end
 
 	if LastSeenSettingsCacheDB.mode then
 		UIDropDownMenu_SetText(settingsFrame.modeDropDown, LastSeenSettingsCacheDB.mode);
 	end
 	-- Synopsis: Sets the value of the mode dropdown to whatever the settings cache holds for that value.
 	
-	--
 	settingsFrame.modeDropDown:SetScript("OnEnter", function(self)
-		GameTooltip_SetDefaultAnchor(GameTooltip, UIParent);
+		GameTooltip:SetOwner(self, "ANCHOR_RIGHT");
 		GameTooltip:SetText("|cffffffff" .. L["DEBUG_MODE"] .. "|r: " .. L["DEBUG_MODE_DESC"] .. 
 		"|cffffffff" .. L["NORMAL_MODE"] .. "|r: " .. L["NORMAL_MODE_DESC"] .. 
 		"|cffffffff" .. L["QUIET_MODE"] .. "|r: " .. L["QUIET_MODE_DESC"]);
@@ -119,7 +146,9 @@ local function SettingsMenu_OnShow()
 	end);
 	
 	settingsFrame.modeDropDown:SetScript("OnLeave", function(self)
-		GameTooltip:Hide();
+		if GameTooltip:GetOwner() == self then
+			GameTooltip:Hide();
+		end
 	end);
 	-- Synopsis: The above two code blocks are what show and hide the mode descriptions when a player hoves over the dropdown.
 
@@ -137,7 +166,7 @@ end
 
 addonTbl.LoadSettings = function(doNotOpen)
 	if doNotOpen then
-		LastSeenSettingsCacheDB = {mode = GetOptions("mode"), rarity = GetOptions("rarity"), lootFast = GetOptions("lootFast")};
+		LastSeenSettingsCacheDB = {mode = GetOptions("mode"), rarity = GetOptions("rarity"), lootFast = GetOptions("lootFast"), showSources = GetOptions("showSources")};
 	else
 		if areOptionsOpen then
 			SettingsMenu_OnClose();
