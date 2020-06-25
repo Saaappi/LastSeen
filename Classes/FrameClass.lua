2584bfb2129d76ecfcd5007f20d6da7c7e3c38a1
Alex <lightskywrites@gmail.com>
2020-06-25 04:54:29 -0500

    More class stuff.

--- Classes/FrameClass.lua
+++ Classes/FrameClass.lua
@@ -10,17 +10,27 @@ addonTbl.Frame = function(variableName, frameName)
 	if not addonTbl.variableName then
 		addonTbl.variableName = CreateFrame("Frame", frameName, UIParent, "BasicFrameTemplateWithInset");
 	else
-		addonTbl.Show(variableName);
+		addonTbl.Show(variableName, 200, 125);
 	end
 end
 -- Synopsis: Responsible for building a frame.
 
-addonTbl.Show = function(variableName)
+addonTbl.Show = function(variableName, height, width)
 	if isFrameVisible then
 		addonTbl.Hide(variableName);
 	else
 		isFrameVisible = true;
-		print("Frame shown");
+		if variableName then
+			variableName:SetMovable(true);
+			variableName:EnableMouse(true);
+			variableName:RegisterForDrag("LeftButton");
+			variableName:SetScript("OnDragStart", variableName.StartMoving);
+			variableName:SetScript("OnDragStop", variableName.StopMovingOrSizing);
+			variableName:SetSize(height, width);
+			variableName:ClearAllPoints();
+			variableName:SetPoint("CENTER", WorldFrame, "CENTER");
+		end
+		addonTbl.Widget(title, "FontString", L["RELEASE"] .. L["ADDON_NAME_SETTINGS"], variableName);
 		addonTbl.variableName:Show();
 	end
 end
@@ -28,7 +38,6 @@ end
 
 addonTbl.Hide = function(variableName)
 	isFrameVisible = false;
-	print("Frame hidden");
 	addonTbl.variableName:Hide();
 end
 -- Synopsis: Hides the provided frame from the screen.
--- Classes/WidgetClass.lua
+++ Classes/WidgetClass.lua
@@ -1,21 +1,29 @@
 -- Namespace Variables
 local addon, addonTbl = ...;
 
-addonTbl.Widget = function(name, type, frameName)
+addonTbl.Widget = function(name, type, text, frameName)
 	if type == "Button" then
 	elseif type == "DropDownMenu" then
 	elseif type == "FontString" then
-		if not 
+		frameName.name = frameName:CreateFontString(nil, "OVERLAY");
+		frameName.name:SetFontObject("GameFontHighlight");
+		frameName.name:SetText(text);
 	end
 end
 -- Synopsis: Creates a widget.
 --[[
 	name: 		Widget's name
 	type: 		Widget's type (see below for supported widget types)
+	text:		The text, if necessary, to use (eg. button or fontstring)
 	frameName: 	Name of the frame the widget will be added to after its creation.
 ]]
 
-addonTbl.AddWidget = function(widgetName)
+addonTbl.AddWidget = function(name, frameName)
+	if not frameName.name then
+		if name == "title" then
+			frameName.name:SetPoint("CENTER", frameName.TitleBg, "CENTER", 5, 0);
+		end
+	end
 end
 -- Synopsis: Places a widget on a frame.
 
