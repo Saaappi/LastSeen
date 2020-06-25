2b8e58397e30aa6516b416798b30068fe75d3232
Alex <lightskywrites@gmail.com>
2020-06-25 04:43:39 -0500

    Small comment block for explaining args to addonTbl.Widget.

--- Classes/WidgetClass.lua
+++ Classes/WidgetClass.lua
@@ -1,13 +1,19 @@
 -- Namespace Variables
 local addon, addonTbl = ...;
 
-addonTbl.Widget = function(name, type)
+addonTbl.Widget = function(name, type, frameName)
 	if type == "Button" then
 	elseif type == "DropDownMenu" then
 	elseif type == "FontString" then
+		if not 
 	end
 end
 -- Synopsis: Creates a widget.
+--[[
+	name: 		Widget's name
+	type: 		Widget's type (see below for supported widget types)
+	frameName: 	Name of the frame the widget will be added to after its creation.
+]]
 
 addonTbl.AddWidget = function(widgetName)
 end
