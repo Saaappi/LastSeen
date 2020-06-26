a57b78cb738c90f4d298f745fc55c26c0a8140e2
Alex <lightskywrites@gmail.com>
2020-06-26 05:35:03 -0500

    The dropdown menu works as expected. Now to get it in the right position...

--- Classes/Widget.lua
+++ Classes/Widget.lua
@@ -2,14 +2,17 @@
 local addon, addonTbl = ...;
 
 -- Module-Local Variables
+local dropDownButtons;
 local L = addonTbl.L;
-local list = UIDropDownMenu_CreateInfo();
 
-local function OnClick(self, arg1)
-	--LastSeenSettingsCacheDB["mode"] = arg1; addonTbl["mode"] = arg1;
-	UIDropDownMenu_SetText(self, arg1);
+local function OnClick(self, buttonText)
+	UIDropDownMenu_SetText(dropDownButtons.parent, buttonText);
 end
 -- Synopsis: Changes the value of the mode dropdown to whatever the player selects.
+--[[
+	self: 			The button object within the dropdown menu
+	buttonText:		This is what the button is supposed to say (eg. Debug, Normal, Quiet)
+]]
 
 addonTbl.CreateWidget = function(name, type, text, frameName, point, parent, relativePos, xOffset, yOffset, height, width)
 	if not height and width then height = 0; width = 0; end; -- Height and Width are optional arguments
@@ -21,29 +24,15 @@ addonTbl.CreateWidget = function(name, type, text, frameName, point, parent, rel
 		frameName[name] = CreateFrame("Frame", name, parent, "UIDropDownMenuTemplate");
 		frameName[name]:SetPoint(point, parent, relativePos, xOffset, yOffset);
 		frameName[name]:SetSize(width, height);
-		frameName[name].initialize = function(self, level)
-		
+		frameName[name].initialize = function(name, level)
+			dropDownButtons = UIDropDownMenu_CreateInfo();
+			dropDownButtons.parent = name;
+			dropDownButtons.func = OnClick;
+			
 			for i = 1, #addonTbl.MODE_DROPDOWN do
-				list.text = addonTbl.MODE_DROPDOWN[i];
-				list.func = ModeDropDownMenu_OnClick;
-				list.arg1 = addonTbl.MODE_DROPDOWN[i];
-				UIDropDownMenu_AddButton(list, level);
+				dropDownButtons.text, dropDownButtons.arg1 = addonTbl.MODE_DROPDOWN[i], addonTbl.MODE_DROPDOWN[i];
+				UIDropDownMenu_AddButton(dropDownButtons);
 			end
-			
-			--[[list.text = L["DEBUG_MODE"];
-			list.func = ModeDropDownMenu_OnClick;
-			list.arg1 = L["DEBUG_MODE"];
-			UIDropDownMenu_AddButton(list, level);
-			
-			list.text = L["NORMAL_MODE"];
-			list.func = ModeDropDownMenu_OnClick;
-			list.arg1 = L["NORMAL_MODE"];
-			UIDropDownMenu_AddButton(list, level);
-
-			list.text = L["QUIET_MODE"];
-			list.func = ModeDropDownMenu_OnClick;
-			list.arg1 = L["QUIET_MODE"];
-			UIDropDownMenu_AddButton(list, level);]]
 			
 		end
 	elseif type == "FontString" then
