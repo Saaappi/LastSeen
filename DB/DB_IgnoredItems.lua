e51e42f6316cd3a095ccf4e7adbc9ac8eb0c75d6
Alex <lightskywrites@gmail.com>
2020-06-26 17:31:03 -0500

    Now ignoring the Tomes of the Tranquil Mind.

--- DB/DB_IgnoredItems.lua
+++ DB/DB_IgnoredItems.lua
@@ -10,6 +10,7 @@ local ignoredItems = {
     [21324] 		= "Yellow Qiraji Resonating Crystal",
 	[137642]		= "Mark of Honor",
 	[141605] 		= "Flight Master's Whistle",
+	[143785]		= "Tome of the Tranquil Mind",
 	[163611]		= "Seafarer's Coin Pouch",
 	[163612]		= "Wayfinder's Satchel",
 	[163613]		= "Sack of Plunder",
