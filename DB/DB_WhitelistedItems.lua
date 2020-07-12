9015d4955d27c0b450443193c76c1a78b98767ea
Alex <lightskywrites@gmail.com>
2020-07-12 04:26:08 -0500

    Added a toy to the whitelist.

--- DB/DB_WhitelistedItems.lua
+++ DB/DB_WhitelistedItems.lua
@@ -5,6 +5,7 @@ local whitelistedItems = {
 	[86547]				= "Skyshard",									-- MOUNT
 	[109739]			= "Star Chart",									-- TOY
 	[113375]			= "Vindicator's Armor Polish Kit",				-- TOY
+	[117550]			= "Angry Beehive",								-- TOY
 	[138797]			= "Illusion: Mongoose",							-- ILLUSION
 	[138798]			= "Illusion: Sunfire",                          -- ILLUSION
 	[138799]			= "Illusion: Soulfrost",                        -- ILLUSION
