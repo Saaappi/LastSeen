cc799c36e9b0b917031bdc9a8333df1daad3b20f
Alex <lightskywrites@gmail.com>
2020-10-22 03:37:19 -0500

    Fixed a mistake in map handler after trying to correct a different issue.

--- handlers/MapHandler.lua
+++ handlers/MapHandler.lua
@@ -3,12 +3,12 @@ tbl.GetCurrentMapInfo = function()
 	local id = C_Map.GetBestMapForUnit("player");
 	if id then
 		if LastSeenMapsDB[id] then
-			return LastSeenMapsDB[id]; -- Return the name of the map stored in the table for the given ID.
+			tbl.currentMap = LastSeenMapsDB[id]; -- Return the name of the map stored in the table for the given ID.
 		else
 			local mapInfo = C_Map.GetMapInfo(id);
 			if not mapInfo.mapID then return end
 			LastSeenMapsDB[id] = mapInfo.name; -- Add the new map to the map table for future use.
-			return LastSeenMapsDB[id];
+			tbl.currentMap = mapInfo.name;
 		end
 	else
 		C_Timer.After(3, tbl.GetCurrentMapInfo); -- Recursively call the function every 3 seconds until a map ID is found.
