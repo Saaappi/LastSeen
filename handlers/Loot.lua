0916d09f99f550ae6d1041cd86b50a3c287622aa
Saaappi <alex.metz1024@gmail.com>
2022-10-02 20:30:56 -0700

    Work related to adding/updating source IDs for armor and weapons.

--- handlers/Loot.lua
+++ handlers/Loot.lua
@@ -15,7 +15,11 @@ local isLooting = false -- A boolean to determine if the player is currently loo
 function LastSeen:New(itemId, itemLink, itemName, itemRarity, itemType, itemIcon, lootDate, map, source, playerClass, playerLevel, ...)
 	-- This is a new item, which is an item that we haven't
 	-- seen before on the current account.
-	LastSeenItemDB[itemId] = { link = itemLink, name = itemName, rarity = itemRarity, type = itemType, icon = itemIcon, lootDate = lootDate, map = map, source = source, playerClass = playerClass, playerLevel = playerLevel }
+	if select(2, ...) then
+		LastSeenItemDB[itemId] = { link = itemLink, name = itemName, rarity = itemRarity, type = itemType, icon = itemIcon, lootDate = lootDate, map = map, source = source, playerClass = playerClass, playerLevel = playerLevel, sourceInfo = { [select(2, ...)] = lootDate } }
+	else
+		LastSeenItemDB[itemId] = { link = itemLink, name = itemName, rarity = itemRarity, type = itemType, icon = itemIcon, lootDate = lootDate, map = map, source = source, playerClass = playerClass, playerLevel = playerLevel }
+	end
 	LastSeen:Print("add", itemIcon, select(1, ...), itemLink, source, lootDate, map)
 end
 
@@ -23,12 +27,16 @@ function LastSeen:Update(itemId, itemLink, itemIcon, lootDate, map, source, play
 	-- The item has been seen before and we need to update
 	-- its source information.
 	for _, property in pairs(properties) do
-		LastSeenItemDB[itemId][property.name] = property.value
+		if property.name == "sourceInfo" then
+			LastSeenItemDB[itemId][property.name][select(2, ...)] = property.value
+		else
+			LastSeenItemDB[itemId][property.name] = property.value
+		end
 	end
 	LastSeen:Print("update", itemIcon, select(1, ...), itemLink, source, lootDate, map)
 end
 
-function LastSeen:Item(itemId, itemLink, itemName, itemRarity, itemType, itemIcon, lootDate, map, source, playerClass, playerLevel, action)
+function LastSeen:Item(itemId, itemLink, itemName, itemRarity, itemType, itemIcon, sourceId, lootDate, map, source, playerClass, playerLevel, action)
 	-- This is a staging ground for items. We need to weed
 	-- out the unwanted items (items that aren't new or in
 	-- need of an update.)
@@ -56,6 +64,11 @@ function LastSeen:Item(itemId, itemLink, itemName, itemRarity, itemType, itemIco
 		if LastSeenItemDB[itemId].playerLevel ~= playerLevel then
 			properties[5] = { name = "playerLevel", value = playerLevel }; continue = true
 		end
+		if LastSeenItemDB[itemId]["sourceInfo"] then
+			if LastSeenItemDB[itemId]["sourceInfo"][sourceId] ~= lootDate then
+				properties[6] = { name = "sourceInfo", value = lootDate }; continue = true
+			end
+		end
 	else
 		-- The item doesn't exist.
 		--
@@ -72,14 +85,11 @@ function LastSeen:Item(itemId, itemLink, itemName, itemRarity, itemType, itemIco
 	
 	local collectedIcon = nil
 	if itemType == "Armor" or itemType == "Weapon" then
-		local _, sourceId = C_TransmogCollection.GetItemInfo(itemLink)
-		if sourceId then
-			local sourceInfo = C_TransmogCollection.GetSourceInfo(sourceId)
-			if sourceInfo.isCollected then
-				collectedIcon = known
-			else
-				collectedIcon = unknown
-			end
+		local sourceInfo = C_TransmogCollection.GetSourceInfo(sourceId)
+		if sourceInfo.isCollected then
+			collectedIcon = known
+		else
+			collectedIcon = unknown
 		end
 	end
 	
@@ -88,9 +98,9 @@ function LastSeen:Item(itemId, itemLink, itemName, itemRarity, itemType, itemIco
 		if map == nil then print("'map' is nil for " .. itemLink) end
 		
 		if action == "Update" then
-			LastSeen:Update(itemId, itemLink, itemIcon, lootDate, map, source, playerClass, playerLevel, properties, collectedIcon)
+			LastSeen:Update(itemId, itemLink, itemIcon, lootDate, map, source, playerClass, playerLevel, properties, collectedIcon, sourceId)
 		else
-			LastSeen:New(itemId, itemLink, itemName, itemRarity, itemType, itemIcon, lootDate, map, source, playerClass, playerLevel, collectedIcon)
+			LastSeen:New(itemId, itemLink, itemName, itemRarity, itemType, itemIcon, lootDate, map, source, playerClass, playerLevel, collectedIcon, sourceId)
 		end
 	end
 end
@@ -157,10 +167,11 @@ function LastSeen:GetItemInfo(itemLink, lootSlot)
 						action = "New"
 					end
 					
+					local _, sourceId = C_TransmogCollection.GetItemInfo(itemLink)
 					if LastSeenCreatureDB[itemSourceCreatureId] then
 						-- The item was acquired from a creature logged by
 						-- the addon.
-						LastSeen:Item(itemId, itemLink, itemName, itemRarity, itemType, itemIcon, date(LastSeenDB.DateFormat), addonTable.map, LastSeenCreatureDB[itemSourceCreatureId], (UnitClass("player")), (UnitLevel("player")), action)
+						LastSeen:Item(itemId, itemLink, itemName, itemRarity, itemType, itemIcon, sourceId, date(LastSeenDB.DateFormat), addonTable.map, LastSeenCreatureDB[itemSourceCreatureId], (UnitClass("player")), (UnitLevel("player")), action)
 					end
 				end
 			end
@@ -193,148 +204,3 @@ e:SetScript("OnEvent", function(self, event, ...)
 		isLooting = false
 	end
 end)
-
---[[
-	Note 1: A source ID is a unique identifier for an individual appearance. It's possible for an item to have 2 or more source IDs, and not every
-	ID may be seen. This could be due to it not being in the game as an option OR that it's no longer dropping... only time can tell.
-]]
-
---[[
-	Note 2: The 'known' and 'unknown' assets are from Can I Mog It? A special thanks to the author for the icons.
-]]
-
---[[tbl.New = function(itemID, itemLink, itemName, itemRarity, itemType, itemSubType, itemEquipLoc, itemIcon, currentDate, currentMap, sourceType, source, playerClass, playerLevel)
-
-	if not source or not itemID then return end if tbl.Items[itemID] then return end
-
-	tbl.Items[itemID] = { itemName = itemName, itemLink = itemLink, itemRarity = itemRarity, itemType = itemType, itemSubType = itemSubType, itemEquipLoc = itemEquipLoc, itemIcon = itemIcon, lootDate = currentDate, source = source, 
-	location = currentMap, sourceIDs = {}, lootedBy = {} };
-	
-	if tbl.Contains(tbl.History, nil, "itemLink", itemLink) ~= true then
-		table.insert(tbl.History, 1, {itemLink = itemLink, itemIcon = itemIcon, lootDate = currentDate, source = source, location = currentMap});
-	end
-	
-	tbl.LootTemplate[itemID] = {[source] = 1};
-	
-	local _, sourceID = C_TransmogCollection.GetItemInfo(itemID);
-	if sourceID then
-		tbl.Items[itemID]["sourceIDs"][sourceID] = tbl.L["DATE"];
-	end
-	
-	if sourceID and tbl.Settings["mode"] ~= tbl.L["SILENT"] then
-		if itemType == "Armor" or itemType == "Weapon" then
-			local isAppearanceKnown = C_TransmogCollection.GetSourceInfo(sourceID).isCollected
-			if isAppearanceKnown then
-				print(tbl.L["ADDON_NAME"] .. tbl.L["ADDED"] .. " |TInterface\\Addons\\LastSeen\\Assets\\known:0|t" .. " |T" .. itemIcon .. ":0|t " .. itemLink .. ", " .. source)
-			elseif isAppearanceKnown ~= true and (tbl.classDefaults[tbl.playerClass]["Armor"] == itemSubType or tbl.classDefaults[tbl.playerClass]["Weapons"][itemSubType] == 1) then
-				print(tbl.L["ADDON_NAME"] .. tbl.L["ADDED"] .. " |TInterface\\Addons\\LastSeen\\Assets\\unknown:0|t" .. " |T" .. itemIcon .. ":0|t " .. itemLink .. "*" .. " - " .. source)
-			else
-				print(tbl.L["ADDON_NAME"] .. tbl.L["ADDED"] .. " |TInterface\\Addons\\LastSeen\\Assets\\unknown:0|t" .. " |T" .. itemIcon .. ":0|t " .. itemLink .. ", " .. source)
-			end
-		end
-	elseif tbl.Settings["mode"] ~= tbl.L["SILENT"] then
-		print(tbl.L["ADDON_NAME"] .. tbl.L["ADDED"] .. " |T" .. itemIcon .. ":0|t " .. itemLink .. ", " .. source)
-	end
-	
-	if playerClass and playerLevel then
-		tbl.Items[itemID]["lootedBy"]["playerClass"] = playerClass
-		tbl.Items[itemID]["lootedBy"]["playerLevel"] = playerLevel
-	end
-	
-	tbl.RollHistory();
-	
-	if tbl.Settings["mode"] == tbl.L["DEBUG"] and source ~= tbl.L["AUCTION_HOUSE"] then
-		if tbl.Creatures[tbl.itemSourceCreatureID] then print(tbl.Creatures[tbl.itemSourceCreatureID].unitName) else print(nil) end
-		if tbl.encounterID then print(tbl.Encounters[tbl.encounterID]) else print(nil) end
-		if tbl.Quests[tbl.questID] then print(tbl.Quests[tbl.questID].questTitle) else print(nil) end
-		if tbl.target then print(tbl.target) else print(nil) end
-	end
-end]]
--- Synopsis: Responsible for adding a NEW (not seen before this moment) item to the items table.
-
---[[tbl.Update = function(itemID, itemLink, itemName, itemRarity, itemType, itemSubType, itemEquipLoc, itemIcon, currentDate, currentMap, sourceType, source, playerClass, playerLevel)
-	if not source or not itemID then return end -- For some reason auctions are calling this...
-	
-	local isSourceKnown
-	
-	if tbl.Items[itemID]["itemRarity"] ~= itemRarity then tbl.Items[itemID]["itemRarity"] = itemRarity; tbl.wasUpdated = true end
-	if tbl.Items[itemID]["itemIcon"] ~= itemIcon then tbl.Items[itemID]["itemIcon"] = itemIcon; tbl.wasUpdated = true end
-	if tbl.Items[itemID]["lootDate"] ~= currentDate then tbl.Items[itemID]["lootDate"] = currentDate; tbl.wasUpdated = true end
-	if tbl.Items[itemID]["location"] ~= currentMap then tbl.Items[itemID]["location"] = currentMap; tbl.wasUpdated = true end
-	if tbl.Items[itemID]["source"] ~= source then tbl.Items[itemID]["source"] = source; tbl.wasUpdated = true end
-	if tbl.Items[itemID]["lootedBy"]["playerClass"] ~= playerClass then tbl.Items[itemID]["lootedBy"]["playerClass"] = playerClass; tbl.wasUpdated = true end
-	if tbl.Items[itemID]["lootedBy"]["playerLevel"] ~= playerLevel then tbl.Items[itemID]["lootedBy"]["playerLevel"] = playerLevel; tbl.wasUpdated = true end
-	
-	if tbl.Items[itemID]["itemIcon"] == nil then tbl.Items[itemID]["itemIcon"] = itemIcon end
-	if tbl.Items[itemID]["itemSubType"] == nil then tbl.Items[itemID]["itemSubType"] = itemSubType end
-	if tbl.Items[itemID]["itemEquipLoc"] == nil then tbl.Items[itemID]["itemEquipLoc"] = itemEquipLoc end
-	
-	if tbl.Contains(tbl.History, nil, "itemLink", itemLink) ~= true then
-		table.insert(tbl.History, 1, {itemLink = itemLink, itemIcon = itemIcon, lootDate = currentDate, source = source, location = currentMap});
-	end
-	
-	if tbl.LootTemplate[itemID] then -- The item has been added to the loot template database at some point in the past.
-		for k, v in next, tbl.LootTemplate[itemID] do
-			if (k == source) then -- An existing source was discovered therefore we should increment that source.
-				v = v + 1 tbl.LootTemplate[itemID][k] = v
-				sourceIsKnown = true
-			else
-				sourceIsKnown = false
-			end
-		end
-		
-		if not sourceIsKnown then
-			tbl.LootTemplate[itemID][source] = 1
-			sourceIsKnown = ""; -- Set this boolean equal to a blank string. 
-		end
-	else -- The item exists in the item template database, but hasn't been inserted into the loot template database yet.
-		tbl.LootTemplate[itemID] = {[source] = 1};
-	end
-	
-	local _, sourceID = C_TransmogCollection.GetItemInfo(itemID);
-	local sourceTblSize = table.getn(tbl.Items[itemID]["sourceIDs"]);
-	if sourceID then
-		if (sourceTblSize < 1) then
-			tbl.Items[itemID]["sourceIDs"][sourceID] = currentDate
-		else
-			for k, v in pairs(tbl.Items[itemID]["sourceIDs"]) do
-				if (k == sourceID) then
-					if (v ~= currentDate) then
-						v = currentDate
-					end
-				end
-			end
-		end
-	end
-	if tbl.Settings["mode"] == tbl.L["SILENT"] or tbl.Settings["mode"] == tbl.L["NO_UPDATES"] then
-		-- If the player uses Silent or No Updates mode, then simply mark the wasUpdated boolean as false so it's not reported to them.
-		tbl.wasUpdated = false;
-	end
-	if tbl.wasUpdated then
-		if sourceID then
-			if itemType == "Armor" or itemType == "Weapon" then
-				local isAppearanceKnown = C_TransmogCollection.GetSourceInfo(sourceID).isCollected
-				if isAppearanceKnown then
-					print(tbl.L["ADDON_NAME"] .. tbl.L["UPDATED"] .. " |TInterface\\Addons\\LastSeen\\Assets\\known:0|t" .. " |T" .. itemIcon .. ":0|t " .. itemLink .. ", " .. source)
-				elseif isAppearanceKnown ~= true and (tbl.classDefaults[tbl.playerClass]["Armor"] == itemSubType or tbl.classDefaults[tbl.playerClass]["Weapons"][itemSubType] == 1) then
-					print(tbl.L["ADDON_NAME"] .. tbl.L["UPDATED"] .. " |TInterface\\Addons\\LastSeen\\Assets\\unknown:0|t" .. " |T" .. itemIcon .. ":0|t " .. itemLink .. "*" .. " - " .. source)
-				else
-					print(tbl.L["ADDON_NAME"] .. tbl.L["UPDATED"] .. " |TInterface\\Addons\\LastSeen\\Assets\\unknown:0|t" .. " |T" .. itemIcon .. ":0|t " .. itemLink .. ", " .. source)
-				end
-			end
-		elseif (tbl.Settings["mode"] ~= tbl.L["SILENT"] or tbl.Settings["mode"] ~= tbl.L["NO_UPDATES"]) then
-			print(tbl.L["ADDON_NAME"] .. tbl.L["UPDATED"] .. " |T" .. itemIcon .. ":0|t " .. itemLink .. ", " .. source)
-		end
-		tbl.wasUpdated = false;
-	end
-	
-	tbl.RollHistory();
-	
-	if tbl.Settings["mode"] == tbl.L["DEBUG"] and source ~= tbl.L["AUCTION_HOUSE"] then
-		if tbl.Creatures[tbl.itemSourceCreatureID] then print(tbl.Creatures[tbl.itemSourceCreatureID].unitName) else print(nil) end
-		if tbl.encounterID then print(tbl.Encounters[tbl.encounterID]) else print(nil) end
-		if tbl.Quests[tbl.questID] then print(tbl.Quests[tbl.questID].questTitle) else print(nil) end
-		if tbl.target then print(tbl.target) else print(nil) end
-	end
-end]]
--- Synopsis: Responsible for updating attributes about items (such as the date they were seen) already in the items table.
