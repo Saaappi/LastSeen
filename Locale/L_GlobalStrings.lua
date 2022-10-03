0ef4736d26a18d423345ce7211da27c4d421cef3
Saaappi <alex.metz1024@gmail.com>
2022-10-02 22:11:00 -0700

    A couple localized string updates.

--- Locale/L_GlobalStrings.lua
+++ Locale/L_GlobalStrings.lua
@@ -119,8 +119,8 @@ if (isLocaleSupported) then
 	L_GLOBALSTRINGS["Text.Output.UpdatedItem.Debug.CollectedIcon"]								= "Updated: |T%s:0|t %s%s (Source: %s) (Date: %s) (Map: %s)"
 	L_GLOBALSTRINGS["Text.Output.UpdatedItem.Normal"]											= "Updated: |T%s:0|t %s"
 	L_GLOBALSTRINGS["Text.Output.UpdatedItem.Normal.CollectedIcon"]								= "Updated: |T%s:0|t %s%s"
-	L_GLOBALSTRINGS["Text.Output.Error.AutoLootIsEnabled"] 										= "|cffFF0000ERROR|r: Auto Loot must be disabled before enabling |cffFFD100Scan on Loot|r."
-	L_GLOBALSTRINGS["Text.Output.Tip.EnableAutoLoot"] 											= "|cff00FF00TIP|r: |cffFFD100Scan on Loot|r was disabled. Remember to re-enable Auto Loot!"
+	L_GLOBALSTRINGS["Text.Output.Error.AutoLootIsEnabled"] 										= "|cffFF0000ERROR|r: Auto Loot must be disabled before enabling |cffFFD100Scan on Loot Opened|r."
+	L_GLOBALSTRINGS["Text.Output.Tip.EnableAutoLoot"] 											= "|cff00FF00TIP|r: |cffFFD100Scan on Loot Opened|r was disabled. Remember to re-enable Auto Loot!"
 	-- End: 	Text Output
 	--
 	-- Start: 	Minimap UI Buttons
