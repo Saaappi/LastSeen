c4100f1eee8b9e3ba20721580398cd49a2580005
Saaappi <alex.metz1024@gmail.com>
2023-04-09 23:23:51 -0700

    More updates!

--- handlers/Loot.lua
+++ handlers/Loot.lua
@@ -1,5 +1,112 @@
 local addonName, addonTable = ...
 local frame = CreateFrame("Frame")
+local known = "|TInterface\\Addons\\LastSeen\\Assets\\known:0|t"
+local unknown = "|TInterface\\Addons\\LastSeen\\Assets\\unknown:0|t"
+
+local function CheckData(tab)
+	for _, arg in ipairs(tab) do
+		if arg == nil then
+			return false
+		end
+	end
+	return true
+end
+
+local function ItemExists(itemID)
+	if LastSeenDB.Items[itemID] then
+		return true
+	end
+	return false
+end
+
+function LastSeen:Item(itemID, itemLink, itemName, itemRarity, itemType, itemIcon, sourceID, lootDate, map, source)
+	-- This is a staging ground for items. We need to weed
+	-- out the unwanted items (items that aren't new or in
+	-- need of an update.)
+	if LastSeenDB.Enabled == false or LastSeenDB.Enabled == nil then return false end
+	
+	-- Check if the item is in the Items table.
+	if ItemExists(itemID) then
+		local updated = false
+		local item = LastSeenDB.Items[itemID]
+		
+		-- Check if the loot date property needs an update.
+		if item.lootDate ~= lootDate and lootDate ~= nil then
+			item.lootDate = lootDate
+			updated = true
+		end
+		
+		-- Check if the map property needs an update.
+		if item.map ~= map and map ~= nil then
+			item.map = map
+			updated = true
+		end
+		
+		-- Check if the source property needs an update.
+		if item.source ~= source and source ~= nil then
+			item.source = source
+			updated = true
+		end
+		
+		-- Check to see if the sourceID needs to be updated.
+		-- Some items have multiple sourceIDs, and some of them
+		-- can stop dropping. We would like to keep record of them.
+		if item.sourceInfo then
+			if sourceID ~= 0 and sourceID ~= nil then
+				if item.sourceInfo.sourceID ~= lootDate then
+					item.sourceInfo.sourceID = lootDate
+					updated = true
+				end
+			end
+		end
+		
+		if LastSeenDB.modeID == 1 then
+			if updated then
+				-- The item was updated, so let's print out the information!
+				print(string.format("Updated: %s %s", itemIcon, link))
+			end
+		end
+	else
+		-- This is a new item.
+		local collectedIcon = ""
+		if sourceID ~= 0 then
+			local sourceInfo = C_TransmogCollection.GetSourceInfo(sourceID)
+			if sourceInfo then
+				if sourceInfo.isCollected then
+					collectedIcon = known
+				else
+					collectedIcon = unknown
+				end
+			end
+		end
+		-- Create a temporary table to hold the item's information. We'll use it to check for
+		-- nil data.
+		local temp = {}
+		temp["link"] = link
+		temp["itemName"] = itemName
+		temp["rarity"] = rarity
+		temp["itemType"] = itemType
+		temp["itemIcon"] = itemIcon
+		temp["lootDate"] = lootDate
+		temp["map"] = map
+		temp["source"] = source
+		
+		local continue = CheckData(temp)
+		if continue then
+			-- All the item's information is valid (no nils).
+			LastSeenDB.Items[itemID] = { link = link, name = itemName, rarity = rarity, type = itemType, icon = itemIcon, lootDate = lootDate, map = map, source = source, sourceInfo = { sourceID = lootDate } }
+			
+			-- The item was added, so let's print out the information!
+			if LastSeenDB.modeID == 1 or LastSeenDB.modeID == 2 then
+				if sourceID ~= 0 then
+					print(string.format("Added: %s %s %s", itemIcon, link, collectedIcon))
+				else
+					print(string.format("Added: %s %s", itemIcon, link))
+				end
+			end
+		end
+	end
+end
 
 -- Events to register with the frame.
 frame:RegisterEvent("LOOT_READY")
@@ -27,7 +134,18 @@ frame:SetScript("OnEvent", function(self, event, ...)
 						if LastSeenDB.Filters[itemType] then
 							-- We're dealing with a supported item, so let's proceed.
 							local _, _, _, _, _, npcID = string.split("-", lootSources[i]); npcID = tonumber(npcID)
-							print(string.format("%s was dropped by %s.", link, LastSeenDB.Creatures[npcID]))
+							
+							-- Let's get the source ID (it's like an ID associated to an appearance)
+							-- of the item.
+							local _, sourceID = C_TransmogCollection.GetItemInfo(link)
+							
+							-- If the sourceID is nil, then it's likely an item without one. Let's
+							-- set it to 0 in those cases.
+							if sourceID == nil then
+								sourceID = 0
+							end
+							
+							LastSeen:Item(itemID, itemLink, itemName, itemRarity, itemType, itemIcon, sourceID, date(LastSeenDB.DateFormat), "Valley of the Four Winds", LastSeenDB.Creatures[npcID])
 						else
 							-- If the mode is set to Normal or Only New then print a statement
 							-- to the player.
@@ -36,31 +154,6 @@ frame:SetScript("OnEvent", function(self, event, ...)
 							end
 						end
 					end
-					
-					-- Let's get some information about the source we acquired
-					-- the item from.
-					--[[
-					
-					if itemID then
-						itemsToSource[itemID] = creatureId
-						local itemSourceCreatureId = itemsToSource[itemID]
-						
-						local action = ""
-						if LastSeenItemDB[itemID] then
-							-- This item has been seen before.
-							action = "Update"
-						else
-							-- This is a new item.
-							action = "New"
-						end
-						
-						local _, sourceId = C_TransmogCollection.GetItemInfo(itemLink)
-						if LastSeenCreatureDB[itemSourceCreatureId] then
-							-- The item was acquired from a creature logged by
-							-- the addon.
-							LastSeen:Item(itemID, itemLink, itemName, rarity, itemType, itemIcon, sourceId, date(LastSeenDB.DateFormat), addonTable.map, LastSeenCreatureDB[itemSourceCreatureId], (UnitClass("player")), (UnitLevel("player")), creatureId, action)
-						end
-					end]]
 				end
 			end
 		end
@@ -69,8 +162,6 @@ end)
 
 
 --[[ OLD CODE
-local known = "|TInterface\\Addons\\LastSeen\\Assets\\known:0|t"
-local unknown = "|TInterface\\Addons\\LastSeen\\Assets\\unknown:0|t"
 
 local isLooting = false -- A boolean to determine if the player is currently looting.
 
@@ -101,88 +192,6 @@ function LastSeen:Update(itemId, itemLink, itemIcon, lootDate, map, source, play
 		end
 	end
 	LastSeen:Print("update", itemIcon, select(1, ...), itemLink, source, lootDate, map)
-end
-
-function LastSeen:Item(itemId, itemLink, itemName, itemRarity, itemType, itemIcon, sourceId, lootDate, map, source, playerClass, playerLevel, creatureId, action)
-	-- This is a staging ground for items. We need to weed
-	-- out the unwanted items (items that aren't new or in
-	-- need of an update.)
-	if LastSeenDB.Enabled == false or LastSeenDB.Enabled == nil then return false end
-	
-	-- Determines whether or not the item should be processed.
-	local continue = false
-	
-	-- Let's see if the item isn't new. If not, then
-	-- let's check to see if any properties are different.
-	local properties = {}
-	if LastSeenItemDB[itemId] then
-		if LastSeenItemDB[itemId].lootDate ~= lootDate then
-			properties[1] = { name = "lootDate", value = lootDate }; continue = true
-		end
-		if LastSeenItemDB[itemId].map ~= map then
-			properties[2] = { name = "map", value = map }; continue = true
-		end
-		if LastSeenItemDB[itemId].source ~= source then
-			properties[3] = { name = "source", value = source }; continue = true
-		end
-		if LastSeenItemDB[itemId].playerClass ~= playerClass then
-			properties[4] = { name = "playerClass", value = playerClass }; continue = true
-		end
-		if LastSeenItemDB[itemId].playerLevel ~= playerLevel then
-			properties[5] = { name = "playerLevel", value = playerLevel }; continue = true
-		end
-		if LastSeenItemDB[itemId]["sourceInfo"] then
-			if LastSeenItemDB[itemId]["sourceInfo"][sourceId] ~= lootDate then
-				properties[6] = { name = "sourceInfo", value = lootDate }; continue = true
-			end
-		end
-	else
-		-- The item doesn't exist.
-		--
-		-- Let's check if the item rarity is equal to or
-		-- higher than the player-controlled setting.
-		if itemRarity < LastSeenDB.RarityId then return end
-		
-		-- Let's check if the item's type is enabled in the
-		-- settings.
-		if LastSeenDB.Filters[itemType] then
-			continue = true
-		end
-	end
-	
-	local collectedIcon = nil
-	if itemType == "Armor" or itemType == "Weapon" then
-		local sourceInfo = C_TransmogCollection.GetSourceInfo(sourceId)
-		if sourceInfo then
-			if sourceInfo.isCollected then
-				collectedIcon = known
-			else
-				collectedIcon = unknown
-			end
-		end
-	end
-	
-	if continue then
-		-- If the item is meant to be added to the table,
-		-- then let's add or update the item in the loot
-		-- template database.
-		if LastSeenLootTemplateDB[itemId] then
-			if not LastSeenLootTemplateDB[itemId][creatureId] then
-				LastSeenLootTemplateDB[itemId][creatureId] = source
-			end
-		else
-			LastSeenLootTemplateDB[itemId] = { [creatureId] = source }
-		end
-		
-		-- The map is nil so don't add it to the table.
-		if map == nil then print(string.format(L_GLOBALSTRINGS["Text.Output.Error.MapUnavailable"], itemLink)); return end
-		
-		if action == "Update" then
-			LastSeen:Update(itemId, itemLink, itemIcon, lootDate, map, source, playerClass, playerLevel, properties, collectedIcon, sourceId)
-		else
-			LastSeen:New(itemId, itemLink, itemName, itemRarity, itemType, itemIcon, lootDate, map, source, playerClass, playerLevel, collectedIcon, sourceId)
-		end
-	end
 end
 
 function LastSeen:ExtractItemLink(itemLinkString)
