c95c95b3c4f6e18ca6571cf326891c286022003d
Saaappi <alex.metz1024@gmail.com>
2023-04-09 23:28:59 -0700

    More updates!

--- handlers/Map.lua
+++ handlers/Map.lua
@@ -1,6 +1,14 @@
 local addonName, addonTable = ...
-local L_GLOBALSTRINGS = addonTable.L_GLOBALSTRINGS
 local e = CreateFrame("Frame")
+
+local function GetParentMap(mapID)
+	local map = C_Map.GetMapInfo(mapID)
+	if map.mapType == 3 or map.mapType == 4 then
+		return map
+	else
+		GetParentMap(map.mapID)
+	end
+end
 
 local function GetBestMapForUnit(unit)
 	if UnitAffectingCombat(unit) then
@@ -16,13 +24,20 @@ e:SetScript("OnEvent", function(self, event, ...)
 	if event == "PLAYER_LOGIN" or event == "ZONE_CHANGED_NEW_AREA" then
 		if LastSeenDB.Enabled == false or LastSeenDB.Enabled == nil then return false end
 		
-		local mapInfo = GetBestMapForUnit("player")
-		if mapInfo then
-			if not LastSeenMapDB[mapInfo.mapID] and (mapInfo.mapType == 3 or mapInfo.mapType == 4) then
-				LastSeenMapDB[mapInfo.mapID] = mapInfo.name
+		local map = GetBestMapForUnit("player")
+		if map then
+			if map.mapType == 5 or map.mapType == 6 then
+				-- The map is a micro or orphan zone, so we need to get the
+				-- parent map.
+				map = GetParentMap(map.mapID)
 			end
 			
-			addonTable.map = mapInfo.name
+			-- Log the map to the map table if it's a zone or dungeon map.
+			if not LastSeenDB.Maps[map.mapID] and (map.mapType == 3 or map.mapType == 4) then
+				LastSeenDB.Maps[map.mapID] = map.name
+			end
+			
+			addonTable.map = map.name
 		end
 	end
 end)
