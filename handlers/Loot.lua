2f11832b9e303a2b98265f5c0d1280279dfcda29
Saaappi <alex.metz1024@gmail.com>
2023-04-09 23:36:03 -0700

    More updates!

--- handlers/Loot.lua
+++ handlers/Loot.lua
@@ -84,7 +84,7 @@ function LastSeen:Item(itemID, itemLink, itemName, itemRarity, itemType, itemIco
 		local temp = {}
 		temp["link"] = itemLink
 		temp["itemName"] = itemName
-		temp["rarity"] = rarity
+		temp["rarity"] = itemRarity
 		temp["itemType"] = itemType
 		temp["itemIcon"] = itemIcon
 		temp["lootDate"] = lootDate
@@ -94,7 +94,7 @@ function LastSeen:Item(itemID, itemLink, itemName, itemRarity, itemType, itemIco
 		local continue = CheckData(temp)
 		if continue then
 			-- All the item's information is valid (no nils).
-			LastSeenDB.Items[itemID] = { link = itemLink, name = itemName, rarity = rarity, type = itemType, icon = itemIcon, lootDate = lootDate, map = map, source = source, sourceInfo = { sourceID = lootDate } }
+			LastSeenDB.Items[itemID] = { itemLink = itemLink, itemName = itemName, itemRarity = itemRarity, itemType = itemType, itemIcon = itemIcon, lootDate = lootDate, map = map, source = source, sourceInfo = { sourceID = lootDate } }
 			
 			-- The item was added, so let's print out the information!
 			if LastSeenDB.modeID == 1 or LastSeenDB.modeID == 2 then
@@ -124,12 +124,12 @@ frame:SetScript("OnEvent", function(self, event, ...)
 					-- GUID. Odd entries are GUIDs and even entries are
 					-- the count for what dropped.
 					-- Let's get some information about the item we looted.
-					local itemName, _, rarity = GetItemInfo(itemLink)
+					local itemName, _, itemRarity = GetItemInfo(itemLink)
 					local itemID, itemType, _, _, itemIcon = GetItemInfoInstant(itemLink)
 					
 					-- Make sure the item's rarity is at or above the desired
 					-- rarity filter.
-					if rarity >= LastSeenDB.rarityID then
+					if itemRarity >= LastSeenDB.rarityID then
 						-- Make sure the item's type is supposed to be tracked.
 						if LastSeenDB.Filters[itemType] then
 							-- We're dealing with a supported item, so let's proceed.
@@ -159,138 +159,3 @@ frame:SetScript("OnEvent", function(self, event, ...)
 		end
 	end
 end)
-
-
---[[ OLD CODE
-
-local isLooting = false -- A boolean to determine if the player is currently looting.
-
--- The select(1, ...) is a variable number of arguments. The arguments passed here are
--- as follows:
---
--- 1: collectedIcon - A checkmark if the item's source is collected. An X if the item's
--- source is NOT collected.
-function LastSeen:New(itemId, itemLink, itemName, itemRarity, itemType, itemIcon, lootDate, map, source, playerClass, playerLevel, ...)
-	-- This is a new item, which is an item that we haven't
-	-- seen before on the current account.
-	if select(2, ...) then
-		LastSeenItemDB[itemId] = { link = itemLink, name = itemName, rarity = itemRarity, type = itemType, icon = itemIcon, lootDate = lootDate, map = map, source = source, playerClass = playerClass, playerLevel = playerLevel, sourceInfo = { [select(2, ...)] = lootDate } }
-	else
-		LastSeenItemDB[itemId] = { link = itemLink, name = itemName, rarity = itemRarity, type = itemType, icon = itemIcon, lootDate = lootDate, map = map, source = source, playerClass = playerClass, playerLevel = playerLevel }
-	end
-	LastSeen:Print("add", itemIcon, select(1, ...), itemLink, source, lootDate, map)
-end
-
-function LastSeen:Update(itemId, itemLink, itemIcon, lootDate, map, source, playerClass, playerLevel, properties, ...)
-	-- The item has been seen before and we need to update
-	-- its source information.
-	for _, property in pairs(properties) do
-		if property.name == "sourceInfo" then
-			LastSeenItemDB[itemId][property.name][select(2, ...)] = property.value
-		else
-			LastSeenItemDB[itemId][property.name] = property.value
-		end
-	end
-	LastSeen:Print("update", itemIcon, select(1, ...), itemLink, source, lootDate, map)
-end
-
-function LastSeen:ExtractItemLink(itemLinkString)
-	-- Extracts the item link from the provided string.
-	-- Example: You receive item: [Invincible's Reins]
-	-- (The Invincible's Reins item link would be extracted
-	-- and returned.)
-	
-	if string.find(itemLinkString, L_GLOBALSTRINGS["Constant.LOOT_ITEM_PUSHED_SELF"]) then
-		local extractedLink = string.match(itemLinkString, L_GLOBALSTRINGS["Constant.LOOT_ITEM_PUSHED_SELF"] .. "(.*).");
-		local itemId = GetItemInfoInstant(extractedLink);
-	elseif string.find(itemLinkString, L_GLOBALSTRINGS["Constant.LOOT_ITEM_SELF"]) then
-		local extractedLink = string.match(itemLinkString, L_GLOBALSTRINGS["Constant.LOOT_ITEM_SELF"] .. "(.*).");
-		local itemId = GetItemInfoInstant(extractedLink);
-	end
-	
-	if itemId then
-		local itemLink = select(2, GetItemInfo(itemId))
-	end
-	
-	if itemLink then return itemLink end
-end
-
-function LastSeen:GetItemInfo(itemLink, lootSlot)
-	local itemsToSource = {}
-	local lootSources = { GetLootSourceInfo(lootSlot) }
-
-	if itemLink then
-		if LastSeenDB.ScanOnLootOpenedEnabled == false or LastSeenDB.ScanOnLootOpenedEnabled == nil then
-			-- We only want to do this if Scan on Loot is NOT enabled.
-			local itemLink = LastSeen:ExtractItemLink(L_GLOBALSTRINGS["Constant.LOOT_ITEM_SELF"] .. itemLink)
-		end
-		
-		for i = 1, #lootSources, 2 do
-			-- We skip every other entry in the table because
-			-- every other entry in the table is a loot source
-			-- GUID. Odd entries are GUIDs and even entries are
-			-- the count for what dropped.
-			-- Let's get some information about the item we looted.
-			local itemName, _, itemRarity = GetItemInfo(itemLink)
-			local itemId, itemType, _, _, itemIcon = GetItemInfoInstant(itemLink)
-			
-			-- Let's get some information about the source we acquired
-			-- the item from.
-			local type, _, _, _, _, creatureId = string.split("-", lootSources[i]); creatureId = tonumber(creatureId)
-			
-			if itemId then
-				itemsToSource[itemId] = creatureId
-				local itemSourceCreatureId = itemsToSource[itemId]
-				
-				local action = ""
-				if LastSeenItemDB[itemId] then
-					-- This item has been seen before.
-					action = "Update"
-				else
-					-- This is a new item.
-					action = "New"
-				end
-				
-				local _, sourceId = C_TransmogCollection.GetItemInfo(itemLink)
-				if LastSeenCreatureDB[itemSourceCreatureId] then
-					-- The item was acquired from a creature logged by
-					-- the addon.
-					LastSeen:Item(itemId, itemLink, itemName, itemRarity, itemType, itemIcon, sourceId, date(LastSeenDB.DateFormat), addonTable.map, LastSeenCreatureDB[itemSourceCreatureId], (UnitClass("player")), (UnitLevel("player")), creatureId, action)
-				end
-			end
-		end
-	end
-end
-
-e:RegisterEvent("LOOT_CLOSED")
-e:RegisterEvent("LOOT_OPENED")
-e:RegisterEvent("LOOT_READY")
-e:SetScript("OnEvent", function(self, event, ...)
-	if (event == "LOOT_OPENED" or event == "LOOT_READY") and not isLooting then
-		isLooting = true
-		
-		-- Get the number of loot slots. If that number is less than 1, then
-		-- return (do nothing).
-		local numLootSlots = GetNumLootItems(); if numLootSlots < 1 then return end
-		
-		if LastSeenDB.ScanOnLootOpenedEnabled then
-			for slot = numLootSlots, 1, -1 do
-				LastSeen:GetItemInfo(GetLootSlotLink(slot), slot)
-			end
-		else
-			-- Iterate through the loot window (but do it backward).
-			for slot = numLootSlots, 1, -1 do
-				LastSeen:GetItemInfo(GetLootSlotLink(slot), slot)
-				if C_CVar.GetCVar("autoLootDefault") == 1 then
-					if not IsModifiedClick("AUTOLOOTTOGGLE") then
-						LootSlot(slot)
-					end
-				end
-			end
-		end
-	end
-	if event == "LOOT_CLOSED" then
-		isLooting = false
-	end
-end)
-]]
