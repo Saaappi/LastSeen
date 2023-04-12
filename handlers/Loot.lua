57dd847ec7e811a7b968af3a0f5379456c5b7c7c
Saaappi <alex.metz1024@gmail.com>
2023-04-11 23:03:49 -0700

    More updates!

--- handlers/Loot.lua
+++ handlers/Loot.lua
@@ -100,7 +100,7 @@ function LastSeen:Item(itemID, itemLink, itemName, itemRarity, itemType, itemIco
 				elseif sourceInfo.isCollected == false and C_TransmogCollection.PlayerCanCollectSource(sourceID) then
 					collectedIcon = unknown
 				elseif sourceInfo.isCollected == false and C_TransmogCollection.PlayerCanCollectSource(sourceID) == false then
-					local bindType == select(14, GetItemInfo(itemLink))
+					local bindType = select(14, GetItemInfo(itemLink))
 					if bindType == 2 then
 						collectedIcon == unknown_by_character
 					end
