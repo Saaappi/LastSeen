691e7cc2116af09f8282436d644b46dfac6f5cec
Saaappi <alex.metz1024@gmail.com>
2023-04-12 21:50:24 -0700

    More updates!

--- DB/Spells.lua
+++ DB/Spells.lua
@@ -1,5 +1,6 @@
 local addonName, addonTable = ...
 local targetSpells = {
+	[3365] = "", -- Opening (used by lots of chests and objects in the game)
 	[6478] = "", -- Opening (used by lots of chests and objects in the game)
 }
 addonTable.targetSpells = targetSpells
