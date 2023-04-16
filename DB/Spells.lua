ee5752dd8b34ffa7da0e35fe816a9063d693d9b2
Saaappi <alex.metz1024@gmail.com>
2023-04-15 17:13:58 -0700

    Added another Opening spell.

--- DB/Spells.lua
+++ DB/Spells.lua
@@ -1,6 +1,7 @@
 local addonName, addonTable = ...
 local targetSpells = {
 	[3365] = "", -- Opening (used by lots of chests and objects in the game)
+	[6477] = "", -- Opening (used by lots of chests and objects in the game)
 	[6478] = "", -- Opening (used by lots of chests and objects in the game)
 }
 addonTable.targetSpells = targetSpells
