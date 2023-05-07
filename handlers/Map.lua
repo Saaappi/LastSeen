b7fdb4ddc0dcc8d5333278186f14cd6f78857755
Saaappi <alex.metz1024@gmail.com>
2023-05-07 15:18:38 -0700

    .

--- handlers/Map.lua
+++ handlers/Map.lua
@@ -1,5 +1,6 @@
 local addonName, addonTable = ...
 local e = CreateFrame("Frame")
+local isOnLoadScreen = false
 
 local function GetCombatStatus(unit)
 	C_Timer.After(0.5, function()
@@ -45,13 +46,32 @@ function LastSeen:GetBestMapForUnit(unit)
 end
 
 e:RegisterEvent("PLAYER_LOGIN")
+e:RegisterEvent("LOADING_SCREEN_ENABLED")
+e:RegisterEvent("LOADING_SCREEN_DISABLED")
+e:RegisterEvent("PLAYER_LOGIN")
 e:RegisterEvent("ZONE_CHANGED")
 e:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 e:SetScript("OnEvent", function(self, event, ...)
 	if (event == "PLAYER_LOGIN") or (event == "ZONE_CHANGED") or (event == "ZONE_CHANGED_NEW_AREA") then
 		if LastSeenDB.Enabled == false or LastSeenDB.Enabled == nil then return false end
+		if isOnLoadScreen then return end
 		
 		C_Timer.After(0.5, function()
+			local map = LastSeen:GetBestMapForUnit("player")
+			if (map) then
+				if (not LastSeenDB.Maps[map.mapID] and (map.mapType == 3 or map.mapType == 4)) then
+					LastSeenDB.Maps[map.mapID] = map.name
+				end
+				addonTable.map = map.name
+			end
+		end)
+	end
+	if (event == "LOADING_SCREEN_ENABLED") then
+		isOnLoadScreen = true
+	end
+	if (event == "LOADING_SCREEN_DISABLED") then
+		C_Timer.After(5, function()
+			isOnLoadScreen = false
 			local map = LastSeen:GetBestMapForUnit("player")
 			if (map) then
 				if (not LastSeenDB.Maps[map.mapID] and (map.mapType == 3 or map.mapType == 4)) then
