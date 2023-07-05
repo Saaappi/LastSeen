e2538ee9d87328354b99c2fd368861a1d11fd878
Saaappi <alex.metz1024@gmail.com>
2023-07-04 21:53:31 -0700

    Changed the changelog a tiny bit.

--- Settings.lua
+++ Settings.lua
@@ -448,7 +448,7 @@ local mainOptions = {
 					type = "header",
 				},
 				fixedText = {
-					name = coloredDash .. "An error should no longer be thrown for druids that Dreamwalk to the Dreamgrove.",
+					name = coloredDash .. "An error should no longer be thrown for druids when dreamwalking to the Dreamgrove.",
 					order = 31,
 					type = "description",
 					fontSize = "medium",
