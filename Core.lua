e1aa92af5dc8a80d3d593c240554883564f49fb5
Saaappi <alex.metz1024@gmail.com>
2024-11-02 04:01:33 -0700

    Removed the debug line.

--- Core.lua
+++ Core.lua
@@ -66,8 +66,7 @@ local function OnEvent(_, event, ...)
     if event == "LOOT_READY" then
         if encounterInProgress then return end -- To prevent encounter loot from logging twice
         local currentTime = GetTime()
-        --if currentTime < (lastTime + 1) then return end -- To prevent multiple LOOT_READY events that fire in the same frame from being processed simultaneously
-        if currentTime < (lastTime + 1) then print("Duplicate LOOT_READY event detected. Skipping...") return end
+        if currentTime < (lastTime + 1) then return end -- To prevent multiple LOOT_READY events that fire in the same frame from being processed simultaneously
         if currentTime > (lastTime + 1) then lastTime = currentTime end
 
         for i=1,GetNumLootItems() do
