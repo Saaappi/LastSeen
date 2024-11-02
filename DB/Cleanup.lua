19cd95cd9698d4ecc0caf9ab339c369ab585c720
Saaappi <alex.metz1024@gmail.com>
2024-11-02 05:48:58 -0700

    Removed most of the old files.

--- DB/Cleanup.lua
+++
@@ -1,9 +0,0 @@
-local addonName, addon = ...
-
-local oldSettings = {
-	["minimapPos"] = true,
-	["RarityId"] = true,
-	["ModeId"] = true,
-	["ShowExtraSourcesEnabled"] = true,
-}
-addon.oldSettings = oldSettings
