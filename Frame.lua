42bd83f889dbed16080c9727ec8e71a3ed6ff66d
Saaappi <alex.metz1024@gmail.com>
2024-11-07 01:30:17 -0800

    More frame work, but it's not where I need it.

--- Frame.lua
+++ Frame.lua
@@ -1,7 +1,7 @@
 local addonName, LastSeen = ...
 
---[[local frame = CreateFrame("Frame", nil, UIParent, "PortraitFrameTemplate")
-frame:SetSize(500, 300)
+local frame = CreateFrame("Frame", nil, UIParent, "PortraitFrameTemplate")
+frame:SetSize(650, 400)
 frame:SetPoint("CENTER", UIParent, "CENTER")
 
 -- Make the frame movable.
@@ -16,7 +16,7 @@ end)
 -- Make sure the frame can't be moved off screen.
 frame:SetClampedToScreen(true)
 
-frame:SetPortraitToAsset(237297)]]
+frame:SetPortraitToAsset(134442)
 
 --[[function LastSearchSearchFrameMixin:OnShow()
     self:SetPortraitToAsset(237297)
@@ -29,3 +29,22 @@ end
 function LastSeenSearchFrameMixin:OnLoad()
     --
 end]]
+
+--[[local SearchBox = CreateFrame("EditBox", nil, UIParent, "SearchBoxTemplate")
+SearchBox:SetPoint("BOTTOMRIGHT", ScrollBox, "TOPRIGHT", 0, 5)
+SearchBox:SetAutoFocus(false)
+SearchBox:SetSize(145, 24)]]
+
+local ScrollBox = CreateFrame("Frame", nil, frame, "WowScrollBoxList")
+ScrollBox:SetPoint("TOPLEFT", frame, "TOPLEFT")
+ScrollBox:SetSize(frame:GetWidth()-20, 250)
+
+local ScrollBar = CreateFrame("EventFrame", nil, UIParent, "MinimalScrollBar")
+ScrollBar:SetPoint("TOPLEFT", ScrollBox, "TOPRIGHT")
+ScrollBar:SetPoint("BOTTOMLEFT", ScrollBox, "BOTTOMRIGHT")
+
+local DataProvider = CreateDataProvider()
+local ScrollView = CreateScrollBoxListLinearView()
+ScrollView:SetDataProvider(DataProvider)
+
+ScrollUtil.InitScrollBoxListWithScrollBar(ScrollBox, ScrollBar, ScrollView)
